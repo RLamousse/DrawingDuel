cf37c2decb03db9d108e48d5ac745b20600a5aed
ItsTehStory <philippe.ballandras@hotmail.com>
2019-01-24 15:47:29 -0500

    Done trivial comparison and started applyMask

--- server/app/services/bitmap-diff.service.ts
+++ server/app/services/bitmap-diff.service.ts
@@ -8,12 +8,38 @@ export class BitmapDiffService {
     public getDiff(sourceImage: Bitmap, modifiedImage: Bitmap) {
 
         let success: Boolean = true;
+        let diffMap: Number[][] = [[]];
 
         if (sourceImage.width !== modifiedImage.width || sourceImage.height !== modifiedImage.height) {
             throw new Error("Cannot generate the difference if the images does not have the same dimensions");
         }
 
+        for (let i = 0; i < sourceImage.pixels.length; i++) {
+            for (let j = 0; j < sourceImage[i].length; j++) {
+                const source = sourceImage[i][j];
+                const modif  = modifiedImage[i][j];
+                diffMap[i][j] = source === modif ? 0xFFFFFF : 0;
+                this.applyMask(diffMap, i, j);
+            }
+        }
+
         return success;
+    }
+
+    private applyMask(image: Number[][], x_index: Number, y_index: Number, mask?: Number[][]) {
+        if (!mask) {
+            mask = [
+                [0, 0, 1, 1, 1, 0, 0],
+                [0, 1, 1, 1, 1, 1, 0],
+                [1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1],
+                [0, 1, 1, 1, 1, 1, 0],
+                [0, 0, 1, 1, 1, 0, 0],
+            ]
+        }
+        
+
     }
 
 }
