cf16449c08cb6eddd427c42d2353054eee782d4a
Milen Bozhilov <milen-irenov.bozhilov@polymtl.ca>
2019-01-26 18:05:58 -0500

    Removing testing code

--- server/app/services/game-creator.service.ts
+++ server/app/services/game-creator.service.ts
@@ -1,27 +1,15 @@
 import { injectable } from "inversify";
 import "reflect-metadata";
 import { Message } from "../../../common/communication/message";
-import {MODIFIED_IMAGE_IDENTIFIER, ORIGINAL_IMAGE_IDENTIFIER} from "../controllers/game-creator.controller";
 
 @injectable()
 export class GameCreatorService {
 
     public async createSimpleGame(gameName: string, originalImage: string, modifiedImage: string): Promise<Message> {
 
-        try {
-            const test: object = {head: "h", body: "b"};
-
-            return {
-                title: test["head2"],
-                body: test["body"],
-            };
-        } catch (error) {
-            return {
-                title: "0",
-                body: "0",
-            };
-        }
-
-
+        return {
+            title: "h",
+            body: "b",
+        };
     }
 }
