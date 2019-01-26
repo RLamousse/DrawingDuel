c5364512ae53332bd6f8869e0732cd37aea17787
Milen Bozhilov <milen-irenov.bozhilov@polymtl.ca>
2019-01-26 18:03:10 -0500

    Isolating game creator service role

--- server/app/controllers/game-creator.controller.ts
+++ server/app/controllers/game-creator.controller.ts
@@ -2,13 +2,15 @@ import { NextFunction, Request, Response, Router } from "express";
 import e = require("express");
 import { inject, injectable } from "inversify";
 import * as multer from "multer";
+import {Message} from "../../../common/communication/message";
 import { GameCreatorService } from "../services/game-creator.service";
 import Types from "../types";
 
 export const UPLOAD_PATH: string = "images/";
-const EXPECTED_FILES_FORMAT: string = "image/bmp";
 export const ORIGINAL_IMAGE_IDENTIFIER: string = "originalImage";
 export const MODIFIED_IMAGE_IDENTIFIER: string = "modifiedImage";
+const EXPECTED_FILES_FORMAT: string = "image/bmp";
+const FILE_NAME_KEY: string = "filename";
 
 const FILE_FILTER: Function = (req: Request, file: Express.Multer.File, cb: Function) => {
     cb(null, file.mimetype === EXPECTED_FILES_FORMAT);
@@ -27,6 +29,9 @@ const UPLOAD: multer.Instance = multer({ fileFilter: FILE_FILTER, storage: STORA
 const CP_UPLOAD: e.RequestHandler = UPLOAD.fields([{name: ORIGINAL_IMAGE_IDENTIFIER, maxCount: 1},
                                                    {name: MODIFIED_IMAGE_IDENTIFIER, maxCount: 1}]);
 
+const FORMAT_ERROR_MESSAGE: Message = {title: "error", body: "Request sent by the client had the wrong format!"};
+const GAME_NAME_IDENTIFIER: string = "gameName";
+
 @injectable()
 export class GameCreatorController {
 
@@ -38,9 +43,27 @@ export class GameCreatorController {
         router.post("/createSimpleGame",
                     CP_UPLOAD,
                     async (req: Request, res: Response, next: NextFunction) => {
-                        res.json(await this.gameCreatorService.createSimpleGame(req));
-                    });
+            if (!this.validityTest(req)) {
+                res.json(FORMAT_ERROR_MESSAGE);
+            } else {
+                res.json(await this.gameCreatorService.createSimpleGame(
+                    req.body[GAME_NAME_IDENTIFIER],
+                    req.files[ORIGINAL_IMAGE_IDENTIFIER][0][FILE_NAME_KEY],
+                    req.files[MODIFIED_IMAGE_IDENTIFIER][0][FILE_NAME_KEY]));
+            }
+
+        });
 
         return router;
+    }
+
+    private validityTest(req: Request): boolean {
+        try {
+            return (req.files[ORIGINAL_IMAGE_IDENTIFIER].length === 1 &&
+                    req.files[MODIFIED_IMAGE_IDENTIFIER].length === 1 &&
+                    req.body[GAME_NAME_IDENTIFIER] !== "");
+        } catch (error) {
+            return false;
+        }
     }
 }
--- server/app/services/game-creator.service.ts
+++ server/app/services/game-creator.service.ts
@@ -1,36 +1,27 @@
-import {Request} from "express";
 import { injectable } from "inversify";
 import "reflect-metadata";
 import { Message } from "../../../common/communication/message";
 import {MODIFIED_IMAGE_IDENTIFIER, ORIGINAL_IMAGE_IDENTIFIER} from "../controllers/game-creator.controller";
 
-const GAME_NAME_IDENTIFIER: string = "gameName";
-
 @injectable()
 export class GameCreatorService {
 
-    public async createSimpleGame(req: Request): Promise<Message> {
+    public async createSimpleGame(gameName: string, originalImage: string, modifiedImage: string): Promise<Message> {
 
         try {
-            if (req.files[ORIGINAL_IMAGE_IDENTIFIER].length !== 1 ||
-                req.files[MODIFIED_IMAGE_IDENTIFIER].length !== 1 ||
-                req.body[GAME_NAME_IDENTIFIER] === "") {
+            const test: object = {head: "h", body: "b"};
 
-                return{
-                    title: "error",
-                    body: "Request sent by the client had the wrong format!",
-                };
-            }
+            return {
+                title: test["head2"],
+                body: test["body"],
+            };
         } catch (error) {
-            return{
-                title: "error",
-                body: "Request sent by the client had the wrong format!",
+            return {
+                title: "0",
+                body: "0",
             };
         }
 
-        return {
-            title: "1",
-            body: "1",
-        };
+
     }
 }
