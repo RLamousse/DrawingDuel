55e66355690d350cb89b5d067fabba591419709b
Milen Bozhilov <milen-irenov.bozhilov@polymtl.ca>
2019-01-26 22:08:40 -0500

    Created first test

--- server/app/services/sample-service.spec.ts
+++ server/app/services/sample-service.spec.ts
@@ -1,8 +1,20 @@
 import { expect } from "chai";
+import { GameCreatorService } from "./game-creator.service";
+
+const gameCreatorService: GameCreatorService = new GameCreatorService();
 
 describe("A sample service", () => {
-    it("should be logged in the coverage", (done: Function) => {
-        expect(1).to.be.not.null;
+
+    before(() => {
+        // TODO copy bmp files from test/test_files_for_game_creator_service to tmp/
+    });
+
+    it("should throw a format error if the strings are not the name of existing files", (done: Function) => {
+        expect(() => gameCreatorService.createSimpleGame("", "", "")).to.throw();
         done();
+    });
+
+    after(() => {
+        // TODO delete all copied files from the beginning
     });
 });
