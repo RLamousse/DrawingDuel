85cbc87e7028a4eb19913442b653078441064922
Milen Bozhilov <milen-irenov.bozhilov@polymtl.ca>
2019-01-26 22:07:53 -0500

    Making game creator sevice async

--- server/app/services/game-creator.service.ts
+++ server/app/services/game-creator.service.ts
@@ -6,7 +6,7 @@ import {DIFFERENCE_ERROR_MESSAGE, FORMAT_ERROR_MESSAGE, NAME_ERROR_MESSAGE} from
 @injectable()
 export class GameCreatorService {
 
-    public  createSimpleGame(gameName: string, originalImage: string, modifiedImage: string): Message {
+    public async createSimpleGame(gameName: string, originalImage: string, modifiedImage: string): Promise<Message> {
         return {title: "Game name", body: "name of the game that was successfully created"};
     }
 }
