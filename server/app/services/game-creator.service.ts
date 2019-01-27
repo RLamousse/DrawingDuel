7b95ac75b45df4d121f8f088b9a8328db0bb6383
Milen Bozhilov <milen-irenov.bozhilov@polymtl.ca>
2019-01-26 20:55:53 -0500

    Remove unused import

--- server/app/services/game-creator.service.ts
+++ server/app/services/game-creator.service.ts
@@ -1,7 +1,6 @@
 import { injectable } from "inversify";
 import "reflect-metadata";
 import { GameCreationStatus } from "../controllers/game-creator.controller"
-import { Message } from "../../../common/communication/message";
 
 @injectable()
 export class GameCreatorService {
