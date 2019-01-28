6bef49ee18d31772568bd1456f70af75525a5bb2
Milen Bozhilov <milen-irenov.bozhilov@polymtl.ca>
2019-01-28 15:58:37 -0500

    Making data base service functions async (WBL, MIB)

--- server/app/services/data-base.service.ts
+++ server/app/services/data-base.service.ts
@@ -5,11 +5,11 @@ import {Message} from "../../../common/communication/message";
 @injectable()
 export class DataBaseService {
 
-    public post(argument: string): Message {
+    public async post(argument: string): Promise<Message> {
         return{title: "post", body: argument};
     }
 
-    public get(argument: string): Message {
+    public async get(argument: string): Promise<Message> {
         return{title: "get", body: argument};
     }
 }
