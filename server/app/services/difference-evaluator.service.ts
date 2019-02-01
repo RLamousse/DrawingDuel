a1341211a8bf1e7ef04a2ae18f65610ce3a1d1d2
Milen Bozhilov <milen-irenov.bozhilov@polymtl.ca>
2019-02-01 04:28:19 -0500

    difference evaluator finishing

--- server/app/services/difference-evaluator.service.ts
+++ server/app/services/difference-evaluator.service.ts
@@ -4,49 +4,37 @@ import "reflect-metadata";
 @injectable()
 export class DifferenceEvaluatorService {
 
-    public getNDifferences(diffImage: string): number {
-        // TODO get BMP file format from the two philipes
-        // pseudocode: 
-        const pixels: number[][] = [[1, 0, 1, 0, 1],
-                                    [1, 1, 1, 1, 1],
-                                    [1, 0, 1, 0, 1],
-                                    [0, 1, 0, 1, 0],
-                                    [1, 1, 1, 1, 1]];
+    public getNDifferences(pixels: number[][]): number {
 
-
-        let translateTable: Map<number, number[]> = new Map<number, number[]>();
+        const TRANSLATE_TABLE: Map<number, number[]> = new Map<number, number[]>();
         let maxCurrentLabel: number = 0;
-        //move this code in utils, array generator
-        let arrayOfLabels: number[][] = new Array<number[]>(pixels.length);
+        const ARRAY_OF_LABELS: number[][] = new Array<number[]>(pixels.length);
 
-        for (let i: number = 0; i < arrayOfLabels.length; i++) {
-            arrayOfLabels[i] = new Array<number>(pixels[0].length);
+        for (let i: number = 0; i < ARRAY_OF_LABELS.length; i++) {
+            ARRAY_OF_LABELS[i] = new Array<number>(pixels[0].length);
         }
-        for (let i: number = 0; i < arrayOfLabels.length; i++) {
-            for(let j: number = 0; j < arrayOfLabels[0].length; j++) {
-                arrayOfLabels[i][j] = 0;
+        for (const LINE of ARRAY_OF_LABELS) {
+            for (const ELEMENT in LINE) {
+                if (LINE.hasOwnProperty(ELEMENT)) {
+                    LINE[ELEMENT] = 0;
+                }
             }
         }
         for (let i: number = 0; i < pixels.length; i++) {
             for (let j: number = 0; j < pixels[0].length; j++) {
                 if (pixels[i][j]) {
-                    maxCurrentLabel = this.analysePixel(i, j, arrayOfLabels, maxCurrentLabel, translateTable);
+                    maxCurrentLabel = this.analysePixel(i, j, ARRAY_OF_LABELS, maxCurrentLabel, TRANSLATE_TABLE);
                 }
             }
         }
 
-        let totalDifferences: number = maxCurrentLabel;
-        for (let key in translateTable) {
-            totalDifferences -= translateTable[key].length;
-        }
-
-        return totalDifferences;
+        return this.calculateDifference(maxCurrentLabel, TRANSLATE_TABLE);
     }
 
     private manageConflict(upValue: number, leftValue: number, translateTable: Map<number, number[]>): void {
         if (typeof translateTable[Math.min(upValue, leftValue)] === "undefined") {
             translateTable[Math.min(upValue, leftValue)] = [Math.max(upValue, leftValue)];
-        } else if (translateTable[Math.min(upValue, leftValue)].indexOf(Math.max(upValue, leftValue) !== -1)) {
+        } else if (translateTable[Math.min(upValue, leftValue)].indexOf(Math.max(upValue, leftValue) >= 0)) {
             translateTable[Math.min(upValue, leftValue)].push(Math.max(upValue, leftValue));
         }
     }
@@ -74,5 +62,17 @@ export class DifferenceEvaluatorService {
         }
 
         return maxCurrentLabel;
+    }
+
+    private calculateDifference(maxCurrentLabel: number, TRANSLATE_TABLE: Map<number, number[]>): number {
+
+        let totalDifferences: number = maxCurrentLabel;
+        for (const key in TRANSLATE_TABLE) {
+            if (TRANSLATE_TABLE.hasOwnProperty(key)) {
+                totalDifferences -= TRANSLATE_TABLE[key].length;
+            }
+        }
+
+        return totalDifferences;
     }
 }
