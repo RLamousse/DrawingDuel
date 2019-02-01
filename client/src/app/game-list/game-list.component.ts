089514a974934f5f11568205293cdce99324c16e
RLamousse <robin.canton-lamousse@polymtl.ca>
2019-02-01 12:31:00 -0500

    starting to receive games objects from server (RCL)

--- client/src/app/game-list/game-list.component.ts
+++ client/src/app/game-list/game-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { GameComponent } from "./game/game.component";
 import { Game } from "../../../../common/model/game";
+import { HttpClient } from "@angular/common/http";
+import { of, Observable } from "rxjs";
+import { catchError } from "rxjs/operators";
 
 @Component({
   selector: "app-game-list",
@@ -9,8 +12,22 @@ import { Game } from "../../../../common/model/game";
 })
 export class GameListComponent implements OnInit {
   public games: Array<Game> = [];
+  public readonly BASE_URL: string = "https://localhost:3000/api/data-base/get-games";
 
-  constructor() { }
+  public constructor(private http: HttpClient) { }
+
+  public getGames(): Observable<Game[]>{
+    return this.http.get<Game[]>(this.BASE_URL).pipe(
+      catchError(this.handleError<Game[]>("basicGet")),
+    );
+  }
+  
+  private handleError<T>(request: string, result?: T): (error: Error) => Observable<T> {
+
+    return (error: Error): Observable<T> => {
+        return of(result as T);
+    };
+}
 
   ngOnInit() {
     let game: Game = {title: "JEU",
