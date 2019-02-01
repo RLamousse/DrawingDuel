3f5c3d093e1950a67af9e7f56da0adcdbe526320
Milen Bozhilov <milen-irenov.bozhilov@polymtl.ca>
2019-02-01 04:26:50 -0500

    functionnal database implementation

--- server/app/controllers/data-base.controller.ts
+++ server/app/controllers/data-base.controller.ts
@@ -1,7 +1,11 @@
 import { NextFunction, Request, Response, Router } from "express";
+import e = require("express");
 import { inject, injectable } from "inversify";
-import {DataBaseService} from "../services/data-base.service";
+import {DataBaseService, GAME_FIELD, GAME_NAME_FIELD, USER_NAME_FIELD} from "../services/data-base.service";
 import Types from "../types";
+
+export const USERNAME_FORMAT_ERROR_MESSAGE: string = "ERROR: the username has the wrong format!";
+export const GAME_NAME_FORMAT_ERROR_MESSAGE: string = "ERROR: the game name has the wrong format!";
 
 @injectable()
 export class DataBaseController {
@@ -11,32 +15,83 @@ export class DataBaseController {
     public get router(): Router {
         const router: Router = Router();
 
-        router.get("/get",async (req: Request, res: Response, next: NextFunction) => {
-            if (!this.validityTest(req)) {
-                next(new Error("Something went wrong"));
-            } else {
-                // call service
-                res.json(this.dataBaseService.get(req.body));
+        router.post("/add-user", async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                this.testUserName(req);
+                res.json(await this.dataBaseService.addUser(req.body[USER_NAME_FIELD]));
+            } catch (error) {
+                next(error);
             }
 
         });
 
-        router.post("/post",async (req: Request, res: Response, next: NextFunction) => {
-                if (!this.validityTest(req)) {
-                    next(new Error("Something went wrong"));
-                } else {
-                    // call service
-                    res.json(this.dataBaseService.post(req.body));
-                }
+        router.delete("/delete-user", async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                this.testUserName(req);
+                res.json(await this.dataBaseService.deleteUser(req.body[USER_NAME_FIELD]));
+            } catch (error) {
+                next(error);
+            }
 
-            });
+        });
+
+        router.post("/add-game", async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                this.testGame(req);
+                res.json(await this.dataBaseService.addGame(req.body[GAME_FIELD]));
+            } catch (error) {
+                next(error);
+            }
+
+        });
+
+        router.get("/get-games", async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                res.json(await this.dataBaseService.getGames());
+            } catch (error) {
+                next(error);
+            }
+
+        });
+
+        router.get("/get-game", async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                this.testGameName(req);
+                res.json(await this.dataBaseService.getGame(req.body.gameName));
+            } catch (error) {
+                next(error);
+            }
+
+        });
 
         return router;
     }
 
+    private testUserName(req: e.Request): void {
+        if (!this.isStringFieldCorrect(USER_NAME_FIELD, req)) {
+            throw new Error(USERNAME_FORMAT_ERROR_MESSAGE);
+        }
+    }
 
+    //TODO test if game has all its attributes first
+    //TODO complete function with file testing
+    private testGame(req: e.Request): void {
+        if (!this.isStringFieldCorrect("gameName", req.body[GAME_FIELD])) {
+            throw new Error(GAME_NAME_FORMAT_ERROR_MESSAGE);
+        }
 
-    private validityTest(req: Request): boolean {
-        return true;
+    }
+
+    private testGameName(req: Request): void {
+        if (!this.isStringFieldCorrect(GAME_NAME_FIELD, req.body)) {
+            throw new Error(GAME_NAME_FORMAT_ERROR_MESSAGE);
+        }
+
+    }
+
+    private isStringFieldCorrect(filedName: string, item: object): boolean {
+        return (!(typeof item[filedName] === "undefined" ||
+                  typeof item[filedName] !== "string" ||
+                  item[filedName] === ""));
     }
 }
--- server/app/services/data-base.service.ts
+++ server/app/services/data-base.service.ts
@@ -1,9 +1,17 @@
 import { injectable } from "inversify";
-import {Collection, Db, MongoClient, MongoError} from "mongodb";
+import {Collection, Db, InsertOneWriteOpResult, MongoClient, MongoError} from "mongodb";
 import "reflect-metadata";
-import {Message} from "../../../common/communication/message";
 import {Game} from "../../../common/Object/game";
-// import {Client} from "socket.io";
+import {Message} from "../../../common/communication/message";
+
+export const ALREADY_EXISTING_USER_MESSAGE_ERROR: string = "ERROR: the specified usename already exists!";
+export const ALREADY_EXISTING_GAME_MESSAGE_ERROR: string = "ERROR: a game with the same name already exists!";
+export const NOT_EXISTING_USER_MESSAGE_ERROR: string = "ERROR: the specified usename does no exist!";
+export const NOT_EXISTING_GAME_MESSAGE_ERROR: string = "ERROR: the specified game does no exist!";
+export const DATA_BASE_MESSAGE_ERROR: string = "ERROR: something went wrong with the database!";
+export const USER_NAME_FIELD: string = "userName";
+export const GAME_FIELD: string = "game";
+export const GAME_NAME_FIELD: string = "gameName";
 
 @injectable()
 export class DataBaseService {
@@ -16,28 +24,108 @@ export class DataBaseService {
     private readonly DB_URL: string = "mongodb://" + this.DB_USER + ":" + this.DB_PASSWORD + "@"
         + this.DB_HOST + ":" + this.DB_PORT + "/" + this.DB_DB;
 
-    private dataBase: Db;
-    private users: Collection<string>;
-    private games: Collection<Game>;
+    private _dataBase: Db;
+    private _users: Collection<string>;
+    private _games: Collection<Game>;
 
     public constructor() {
         MongoClient.connect(this.DB_URL, {useNewUrlParser : true}, (err: MongoError, client: any) => {
             if (!err) {
-                this.dataBase = client.db(this.DB_DB);
-                this.users = this.dataBase.collection("users");
-                this.games = this.dataBase.collection("games");
+                this._dataBase = client.db(this.DB_DB);
+                this._users = this._dataBase.collection("users");
+                this._games = this._dataBase.collection("games");
             } else {
-                console.dir(err);
-                // TODO exit program
+                throw(err);
             }
         });
     }
 
-    public async post(argument: string): Promise<Message> {
-        return{title: "post", body: argument};
+    public async addUser(userName: string): Promise<Message> {
+        if (await this.containsUser(userName)) {
+            throw new Error(ALREADY_EXISTING_USER_MESSAGE_ERROR);
+        } else {
+            return new Promise<Message>((resolve: (value?: Message | PromiseLike<Message>) => void, reject: (reason?: Error) => void) => {
+                // @ts-ignore
+                this._users.insertOne( {[USER_NAME_FIELD]: userName}, (error: MongoError, res: InsertOneWriteOpResult) => {
+                    if (error) {
+                        reject( new Error(DATA_BASE_MESSAGE_ERROR));
+                    }
+                    resolve({title: "User added", body: "user " + userName + " successfully added!"});
+                });
+            });
+        }
     }
 
-    public async get(argument: string): Promise<Message> {
-        return{title: "get", body: argument};
+    public async deleteUser(userName: string): Promise<Message> {
+        if (!(await this.containsUser(userName))) {
+            throw new Error(NOT_EXISTING_USER_MESSAGE_ERROR);
+        } else {
+
+            return new Promise<Message>((resolve: (value?: Message | PromiseLike<Message>) => void, reject: (reason?: Error) => void) => {
+                this._users.deleteOne( {[USER_NAME_FIELD]: {$eq : userName}}, (error: MongoError, res: InsertOneWriteOpResult) => {
+                    if (error) {
+                        reject( new Error(DATA_BASE_MESSAGE_ERROR));
+                    }
+                    resolve({title: "User deleted", body: "user " + userName + " successfully deleted!"});
+                });
+            });
+        }
+    }
+
+    private async containsUser(userName: string): Promise<boolean> {
+        try {
+            return ((await this._users.count( {[USER_NAME_FIELD] : {$eq : userName}})) !== 0);
+        } catch (error) {
+            throw new Error(DATA_BASE_MESSAGE_ERROR);
+        }
+    }
+
+    public async addGame(game: Game): Promise<Message> {
+        if (await this.containsGame(game.gameName)) {
+            throw new Error(ALREADY_EXISTING_GAME_MESSAGE_ERROR);
+        } else {
+            return new Promise<Message>((resolve: (value?: Message | PromiseLike<Message>) => void, reject: (reason?: Error) => void) => {
+
+                this._games.insertOne( game, (error: MongoError, res: InsertOneWriteOpResult) => {
+                    if (error) {
+                        reject( new Error(DATA_BASE_MESSAGE_ERROR));
+                    }
+                    resolve({title: "game added", body: "game " + game.gameName + " successfully added"});
+                });
+            });
+        }
+
+    }
+
+    private async containsGame(gameName: string): Promise<boolean> {
+        try {
+            return ((await this._games.count( {[GAME_NAME_FIELD] : {$eq : gameName}})) !== 0);
+        } catch (error) {
+            throw new Error(DATA_BASE_MESSAGE_ERROR);
+        }
+    }
+
+    public async getGames(): Promise<Game[]> {
+        return new Promise<Game[]>((resolve: (value?: Game[] | PromiseLike<Game[]>) => void, reject: (reason?: Error) => void) => {
+            this._games.find().toArray((error: MongoError, res: Game[]) => {
+                if (error) {
+                    reject( new Error(DATA_BASE_MESSAGE_ERROR));
+                }
+                resolve(res);
+            });
+        });
+    }
+
+    public async getGame(gameName: string): Promise<Game> {
+        return new Promise<Game>((resolve: (value?: Game | PromiseLike<Game>) => void, reject: (reason?: Error) => void) => {
+            this._games.find({[GAME_NAME_FIELD] : {$eq : gameName}}).toArray((error: MongoError, res: Game[]) => {
+                if (error) {
+                    reject( new Error(DATA_BASE_MESSAGE_ERROR));
+                } else if (res.length === 0) {
+                    reject(new Error(NOT_EXISTING_GAME_MESSAGE_ERROR));
+                }
+                resolve(res[0]);
+            });
+        });
     }
 }
