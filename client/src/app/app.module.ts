9adae45a5a6413e4c86358c1fc5907d1b9807ac7
Phil <philippe.ballandras@hotmail.com>
2019-02-01 12:15:35 -0500

    Created the form with ng-material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,21 +1,32 @@
-import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule } from "@angular/material";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialFileInputModule } from "ngx-material-file-input";
 import { AppComponent } from "./app.component";
 import { IndexService } from "./index.service";
-import { HttpClientModule } from "@angular/common/http";
-import { SimpleGameCreatorFormComponent } from './simple-game-creator-form/simple-game-creator-form.component';
+import { SimpleGameCreatorFormComponent } from "./simple-game-creator-form/simple-game-creator-form.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    SimpleGameCreatorFormComponent
+    SimpleGameCreatorFormComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatFormFieldModule,
+    MaterialFileInputModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    BrowserAnimationsModule,
   ],
   providers: [IndexService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- client/src/app/simple-game-creator-form/simple-game-creator-form.component.ts
+++ client/src/app/simple-game-creator-form/simple-game-creator-form.component.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+// import { FileInput } from 'ngx-material-file-input';
 
 @Component({
-  selector: 'app-simple-game-creator-form',
-  templateUrl: './simple-game-creator-form.component.html',
-  styleUrls: ['./simple-game-creator-form.component.css']
+  selector: "app-simple-game-creator-form",
+  templateUrl: "./simple-game-creator-form.component.html",
+  styleUrls: ["./simple-game-creator-form.component.css"],
 })
 export class SimpleGameCreatorFormComponent implements OnInit {
 
-  constructor() { }
+  private formDoc: FormGroup;
+  // private maxSize: number = 100;
 
-  ngOnInit() {
-    
+  public constructor(private _fb: FormBuilder) { }
+
+  public ngOnInit(): void {
+    this.formDoc = this._fb.group({
+      originalImage: [
+        undefined,
+        [Validators.required],
+      ],
+      modifiedImage: [
+        undefined,
+        [Validators.required],
+      ],
+    });
   }
 
+  public checkFileDimension(fileName: string): boolean {
+    this.formDoc.get(fileName);
+
+    return false;
+  }
+
+  public getFormDoc (): FormGroup {
+    return this.formDoc;
+  }
 }
