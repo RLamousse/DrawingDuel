62569a33f3c6d41c213eae635afb49d09f1494e2
ItsTehStory <philippe.ballandras@hotmail.com>
2019-02-02 19:55:29 -0500

    Worked on getting client verification for name and files

--- client/src/app/simple-game-creator-form/simple-game-creator-form.component.ts
+++ client/src/app/simple-game-creator-form/simple-game-creator-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-// import { FileInput } from 'ngx-material-file-input';
+import { FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from "@angular/forms";
+import { Buffer } from "buffer";
+import { getDimensionsFromBuffer } from "../../../../common/image/Bitmap/bitmap-utils";
 
 @Component({
   selector: "app-simple-game-creator-form",
@@ -10,30 +11,70 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 export class SimpleGameCreatorFormComponent implements OnInit {
 
   private formDoc: FormGroup;
-  // private maxSize: number = 100;
+  private readonly MIN_LENGTH: number = 5;
+  private readonly MAX_SIZE: number = 1000000;
 
-  public constructor(private _fb: FormBuilder) { }
+  public constructor(private _fb: FormBuilder) {
+    this.fileValidator = this.fileValidator.bind(this);
+  }
 
   public ngOnInit(): void {
     this.formDoc = this._fb.group({
       originalImage: [
         undefined,
         [Validators.required],
+        this.fileValidator,
       ],
       modifiedImage: [
         undefined,
         [Validators.required],
+        this.fileValidator,
+      ],
+      name: [
+        undefined,
+        [Validators.minLength(this.MIN_LENGTH)],
       ],
     });
   }
 
-  public checkFileDimension(fileName: string): boolean {
-    this.formDoc.get(fileName);
-
-    return false;
+  public exit(): void {
+    this.formDoc.reset();
   }
 
-  public getFormDoc (): FormGroup {
-    return this.formDoc;
+  private async fileValidator(control: FormControl): Promise<ValidationErrors | null> {
+    if (control.value) {
+      const file: File = control.value.files[0];
+      // tslint:disable-next-line:no-console
+      console.log(file);
+      if (file.type !== "image/bmp") {
+        return { image: "L'image doit être de type bmp" };
+      }
+      if (file.size > this.MAX_SIZE) {
+        return { image: "L'image est trop grande" };
+      }
+      const dimensionsOk: boolean = await this.checkFile(file);
+      if (!dimensionsOk) {
+        return {image: "L'image n'est pas de la bonne dimension"};
+      }
+
+      return null;
+    }
+
+    return null;
+  }
+
+  public async checkFile(file: File): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      const reader: FileReader = new FileReader();
+
+      // tslint:disable-next-line:no-any
+      reader.onload = (event: any) => {
+        const data: Buffer = new Buffer(event.target.result);
+        const dimensions: {width: number, height: number} = getDimensionsFromBuffer(data);
+        const imageOk: boolean = dimensions === {width: 640, height: 480};
+        resolve(imageOk);
+      };
+      reader.readAsArrayBuffer(file);
+    });
   }
 }
