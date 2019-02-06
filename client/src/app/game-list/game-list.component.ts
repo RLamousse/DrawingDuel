219b3ba46b32d9792d9b305485c5359e3d5328a6
Robin Canton Lamousse <robin.canton-lamousse@polymtl.ca>
2019-02-05 23:29:10 -0500

    hardcode games

--- client/src/app/game-list/game-list.component.ts
+++ client/src/app/game-list/game-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Game } from "../../../../common/Object/game";
 import { GameService } from "../../app/game.service";
+import { MOCKMIXGAMELIST } from "../mockGames";
 
 @Component({
   selector: "app-game-list",
@@ -16,6 +17,8 @@ export class GameListComponent implements OnInit {
     this.gameService.getGames().subscribe((gamesToModify: Game[]) => {
       this.gameService.convertScoresObject(gamesToModify);
       this.gameService.pushGames(gamesToModify);
+      this.gameService.convertScoresObject(MOCKMIXGAMELIST);
+      this.gameService.pushGames(MOCKMIXGAMELIST);
     });
   }
 }
