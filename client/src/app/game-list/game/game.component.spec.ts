d4faad37fdc8b86800b9c8c97f9d506db48bb033
Philippe Rivest <technophil98@gmail.com>
2019-02-05 22:13:40 -0500

    Fixed game component "should create" test

--- client/src/app/game-list/game/game.component.spec.ts
+++ client/src/app/game-list/game/game.component.spec.ts
@@ -1,6 +1,4 @@
-// tslint:disable:no-magic-numbers
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-
 import { GameComponent } from "./game.component";
 
 describe("GameComponent", () => {
@@ -10,31 +8,12 @@ describe("GameComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ GameComponent ],
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
+    });
     fixture = TestBed.createComponent(GameComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  }));
 
   it("should create", () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeDefined();
   });
-
-  /*it("checks if it generates a number between the min and max", () => {
-    const numArray: number[] = new Array(3) as number[];
-    // component.generateRandomScores(numArray);
-    expect (numArray[0]).toBeGreaterThan(7);
-    expect (numArray[1]).toBeLessThan(25);
-  });
-
-  it("checks if the array of number is sorted", () => {
-    const numArray: number[] = new Array(3) as number[];
-    // component.generateRandomScores(numArray);
-    expect (numArray[0]).toBeLessThan(numArray[1]);
-    expect (numArray[1]).toBeLessThan(numArray[2]);
-  });*/
 });
