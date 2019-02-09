21fe22d099450327c7bee63a4e0a30ef19e42e00
Anthony Potvin <DarkHoTs66@hotmail.com>
2019-02-09 12:29:34 -0500

    no more collision when scene is created, first erson control abse implementation done (AP)

--- client/src/app/scene-creator/scene-renderer.service.ts
+++ client/src/app/scene-creator/scene-renderer.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import * as THREE from 'three';
 import { Form3DService } from "./3DFormService/3-dform.service";
-import { OrbitControls } from 'three-orbitcontrols-ts';
-
+//import { OrbitControls } from 'three-orbitcontrols-ts';
+require('three-first-person-controls')(THREE);
 @Injectable()
 export class SceneRendererService {
 
@@ -12,7 +12,8 @@ export class SceneRendererService {
   private camera: THREE.PerspectiveCamera;
   private renderer: THREE.WebGLRenderer;
   private scene: THREE.Scene;
-  private controls: OrbitControls;
+  //private controls: OrbitControls;
+  private fpControls: THREE.FirstPersonControls;
 
   private lighting: THREE.DirectionalLight;
   private ambiantLight: THREE.AmbientLight;
@@ -25,7 +26,9 @@ export class SceneRendererService {
   private cameraY: number = 0;
   private cameraZ: number = 100;
 
-  private obj3DToCreate: number = 200;
+  private obj3DToCreate: number = 100;
+  private objects: THREE.Mesh[] = [];
+  private minDistCenterObject: number = 43.0;
 
   private setRenderer(): void {
     this.renderer = new THREE.WebGLRenderer();
@@ -33,15 +36,18 @@ export class SceneRendererService {
     this.renderer.setPixelRatio(devicePixelRatio);
     this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
     this.container.appendChild(this.renderer.domElement);
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-
+    //this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.fpControls = new THREE.FirstPersonControls(this.camera, this.renderer.domElement);
+    this.fpControls.movementSpeed = 50;
+    this.fpControls.lookSpeed = 0.05;
     this.renderLoop();
   }
 
   private renderLoop(): void {
     requestAnimationFrame(() => this.renderLoop());
     this.renderer.render(this.scene, this.camera);
-    this.controls.update();
+    //this.controls.update();
+    this.fpControls.update(0.17);
   }
 
   private getRandomValue(min: number, max: number): number {
@@ -49,35 +55,64 @@ export class SceneRendererService {
   }
 
   private create3DObjects() {
-    enum objectGemoetry { sphere, cube, cone, cylinder, pyramid }
+    enum objectGeometry { sphere, cube, cone, cylinder, pyramid }
     let randomObject: number;
     let object: THREE.Mesh;
     for (let i = 0; i < this.obj3DToCreate; ++i) {
       randomObject = this.getRandomValue(0, 4);
       switch (randomObject) {
-        case objectGemoetry.sphere: {
+        case objectGeometry.sphere: {
           object = this.formService.createSphere();
           break;
         }
-        case objectGemoetry.cube: {
+        case objectGeometry.cube: {
           object = this.formService.createCube();
           break;
         }
-        case objectGemoetry.cone: {
+        case objectGeometry.cone: {
           object = this.formService.createCone();
           break;
         }
-        case objectGemoetry.cylinder: {
-          object = this.formService.createSphere();
+        case objectGeometry.cylinder: {
+          object = this.formService.createCylinder();
           break;
         }
-        case objectGemoetry.pyramid: {
+        case objectGeometry.pyramid: {
           object = this.formService.createPyramid();
           break;
         }
+        default: {
+          object = new THREE.Mesh();
+        }
       }
-      object.position.set(this.getRandomValue(-200, 200), this.getRandomValue(-200, 200), this.getRandomValue(-200, 200));
+      let collision = true;
+      let distanceVec: THREE.Vector3;
+      object.position.set(
+        this.getRandomValue(-300, 300),
+        this.getRandomValue(-300, 300),
+        this.getRandomValue(-300, 300)
+      );
       object.rotation.set(this.getRandomValue(0, 360), this.getRandomValue(0, 360), this.getRandomValue(0, 360));
+      if (this.objects.length != 0) {
+        while (collision) {
+          for (let i = 0; i < this.objects.length; ++i) {
+            distanceVec = new THREE.Vector3(
+              this.objects[i].position.x - object.position.x,
+              this.objects[i].position.y - object.position.y,
+              this.objects[i].position.z - object.position.z,
+            );
+            console.log(distanceVec.length());
+            if (distanceVec.length() < this.minDistCenterObject) {
+              object.position.set(this.getRandomValue(-300, 300), this.getRandomValue(-300, 300), this.getRandomValue(-300, 300));
+              object.rotation.set(this.getRandomValue(0, 360), this.getRandomValue(0, 360), this.getRandomValue(0, 360));
+              collision = true;
+              break;
+            } else { collision = false; }
+          }
+        }
+      }
+      
+      this.objects.push(object);
       this.scene.add(object);
     }
   }
