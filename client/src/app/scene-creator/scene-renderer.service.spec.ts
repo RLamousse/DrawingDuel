61bd5553c6fbbb5ba4befac7786dd67d1c4f635f
Anthony Potvin <DarkHoTs66@hotmail.com>
2019-02-12 16:54:17 -0500

    scene creator service now pass (init ok) (AP)

--- client/src/app/scene-creator/scene-renderer.service.spec.ts
+++ client/src/app/scene-creator/scene-renderer.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from "@angular/core/testing";
-
+import { Form3DService } from "./3DFormService/3-dform.service";
 import { SceneRendererService } from "./scene-renderer.service";
 
 describe("SceneRendererService", () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [SceneRendererService, Form3DService],
+  }));
 
   it("should be created", () => {
     const service: SceneRendererService = TestBed.get(SceneRendererService);
