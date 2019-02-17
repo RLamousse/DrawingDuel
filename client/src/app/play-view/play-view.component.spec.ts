7003b375d853b23248d5c048057b439973b6b21d
Ophion <li.wenbo555@hotmail.com>
2019-02-17 17:39:09 -0500

    queryParams unit test starting(falling)

--- client/src/app/play-view/play-view.component.spec.ts
+++ client/src/app/play-view/play-view.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-
+import {ActivatedRoute, Router} from "@angular/router";
+import { Observable } from "rxjs";
 import { PlayViewComponent } from "./play-view.component";
 
 describe("PlayViewComponent", () => {
@@ -9,6 +10,17 @@ describe("PlayViewComponent", () => {
   beforeEach((done) => {
     TestBed.configureTestingModule({
       declarations: [ PlayViewComponent ],
+      providers: [
+        { provide: Router, useClass: class { public navigate: jasmine.Spy = jasmine.createSpy("navigate"); }, },
+        {
+          provide: ActivatedRoute,
+          useValue: {params: {
+            subscribe: (fn: (value: Params) => void) => fn({
+                tab: 0,
+            }),
+        }, } , },
+
+      ],
     });
     done();
   });
