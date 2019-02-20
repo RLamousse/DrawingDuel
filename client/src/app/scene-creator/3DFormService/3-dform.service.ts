a4d4b090da2c03d4af62dd47a003d3c9754f7549
Anthony Potvin <DarkHoTs66@hotmail.com>
2019-02-20 09:22:46 -0500

    [Dev] create3Dform, improve private attribute, QA improved (AP)

--- client/src/app/scene-creator/3DFormService/3-dform.service.ts
+++ client/src/app/scene-creator/3DFormService/3-dform.service.ts
@@ -3,68 +3,61 @@ import * as THREE from "three";
 @Injectable()
 export class Form3DService {
 
-  private baseSize: number;
-  private maxSizeFactor: number;
-  private minSizeFactor: number;
-  private segments: number;
-  private radiusFactor: number;
-  private material: THREE.MeshPhongMaterial;
-  private colorMask: number;
-
-  public constructor() {
-    this.baseSize = 20;
-    this.maxSizeFactor = 150;
-    this.minSizeFactor = 50;
-    this.segments = 32;
-    this.radiusFactor = 2;
-    this.colorMask = 0xFFFFFF;
-  }
+  private readonly BASE_SIZE: number = 20;
+  private readonly MAX_SIZE_FACTOR: number = 150;
+  private readonly MIN_SIZE_FACTOR: number = 50;
+  private readonly SEGMENTS: number = 32;
+  private readonly RADIUS_FACTOR: number = 2;
+  private readonly COLOR_MASK: number = 0xFFFFFF;
 
   private sizeGenerator(): number {
     const percentFactor: number = 100;
 
-    return (Math.floor(Math.random() * (this.maxSizeFactor - this.minSizeFactor + 1) + this.minSizeFactor) / percentFactor * this.baseSize);
+    return (
+      Math.floor(Math.random() * (this.MAX_SIZE_FACTOR - this.MIN_SIZE_FACTOR + 1) + this.MIN_SIZE_FACTOR) /
+      percentFactor * this.BASE_SIZE
+    );
   }
 
   public createCube(): THREE.Mesh {
     const cubeSide: number = this.sizeGenerator();
     const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(cubeSide, cubeSide, cubeSide);
-    this.material = new THREE.MeshPhongMaterial({ color: (Math.random() * this.colorMask) });
+    const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: (Math.random() * this.COLOR_MASK) });
 
-    return new THREE.Mesh(geometry, this.material);
+    return new THREE.Mesh(geometry, material);
   }
 
   public createSphere(): THREE.Mesh {
-    const sphereRadius: number = this.sizeGenerator() / this.radiusFactor;
-    const geometry: THREE.SphereGeometry = new THREE.SphereGeometry(sphereRadius, this.segments, this.segments);
-    this.material = new THREE.MeshPhongMaterial({ color: (Math.random() * this.colorMask) });
+    const sphereRadius: number = this.sizeGenerator() / this.RADIUS_FACTOR;
+    const geometry: THREE.SphereGeometry = new THREE.SphereGeometry(sphereRadius, this.SEGMENTS, this.SEGMENTS);
+    const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: (Math.random() * this.COLOR_MASK) });
 
-    return new THREE.Mesh(geometry, this.material);
+    return new THREE.Mesh(geometry, material);
   }
 
   public createCone(): THREE.Mesh {
     const coneSize: number = this.sizeGenerator();
     const geometry: THREE.ConeGeometry = new THREE.ConeGeometry(
-      coneSize / this.radiusFactor,
+      coneSize / this.RADIUS_FACTOR,
       coneSize,
-      this.segments,
+      this.SEGMENTS,
     );
-    this.material = new THREE.MeshPhongMaterial({ color: (Math.random() * this.colorMask) });
+    const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: (Math.random() * this.COLOR_MASK) });
 
-    return new THREE.Mesh(geometry, this.material);
+    return new THREE.Mesh(geometry, material);
   }
 
   public createCylinder(): THREE.Mesh {
     const cylinderSize: number = this.sizeGenerator();
     const geometry: THREE.CylinderGeometry = new THREE.CylinderGeometry(
-      cylinderSize / this.radiusFactor,
-      cylinderSize / this.radiusFactor,
+      cylinderSize / this.RADIUS_FACTOR,
+      cylinderSize / this.RADIUS_FACTOR,
       cylinderSize,
-      this.segments,
+      this.SEGMENTS,
     );
-    this.material = new THREE.MeshPhongMaterial({ color: (Math.random() * this.colorMask) });
+    const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: (Math.random() * this.COLOR_MASK) });
 
-    return new THREE.Mesh(geometry, this.material);
+    return new THREE.Mesh(geometry, material);
   }
 
   public createPyramid(): THREE.Mesh {
@@ -72,13 +65,13 @@ export class Form3DService {
     const baseSides: number = 3;
     const geometry: THREE.CylinderGeometry = new THREE.CylinderGeometry(
       0,
-      pyramidSize / this.radiusFactor,
+      pyramidSize / this.RADIUS_FACTOR,
       pyramidSize,
       baseSides,
       1,
     );
-    this.material = new THREE.MeshPhongMaterial({ color: (Math.random() * this.colorMask) });
+    const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: (Math.random() * this.COLOR_MASK) });
 
-    return new THREE.Mesh(geometry, this.material);
+    return new THREE.Mesh(geometry, material);
   }
 }
