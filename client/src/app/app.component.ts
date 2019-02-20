24107dcc4ae289e54616bd4a81b9f11e0dc520c4
Phil <philippe.ballandras@hotmail.com>
2019-02-20 12:22:37 -0500

    [Dev]  Added basic client websocket implementation

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { SocketService } from "./socket.service";
+import { SocketEvent } from "../../../common/communication/socket-events";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public readonly title: string = "LOG2990";
+
+  private socket: SocketIOClient.Socket;
+
+  public constructor(private socketService: SocketService) { }
+
+  public ngOnInit(): void {
+      this.connectSocket();
+  }
+
+  public connectSocket(): void {
+    this.socket = this.socketService.openSocket();
+    this.socket.on("connect", () => {
+        this.socket.on(SocketEvent.WELCOME, (data: Object) => {
+            console.log(data);
+        });
+    });
+}
 }
---
+++ client/src/app/socket.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@angular/core";
+import { connect } from "socket.io-client";
+
+@Injectable()
+export class SocketService {
+
+    private readonly BASE_URL: string = "http://localhost:3000/";
+
+    public openSocket(): SocketIOClient.Socket {
+        return connect(this.BASE_URL);
+    }
+}
