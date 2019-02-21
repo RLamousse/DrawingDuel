1af3f416f3f2740c1e940afc3a5c43d2cb4d7e81
Anthony Potvin <DarkHoTs66@hotmail.com>
2019-02-20 23:54:03 -0500

    [Dev] refoctored free-game-creator service of client side and 3d-form service, linting ok, tests need to be rework (AP)

--- client/src/app/scene-creator/3DFormService/3-dform.service.ts
+++ client/src/app/scene-creator/3DFormService/3-dform.service.ts
@@ -1,77 +1,78 @@
 import { Injectable } from "@angular/core";
 import * as THREE from "three";
+import { Coordinate } from "../../../../../common/free-game-json-interface/FreeGameCreatorInterface/free-game-enum";
+import * as IObject from "../../../../../common/free-game-json-interface/JSONInterface/IScenesJSON";
 @Injectable()
 export class Form3DService {
 
-  private readonly BASE_SIZE: number = 20;
-  private readonly MAX_SIZE_FACTOR: number = 150;
-  private readonly MIN_SIZE_FACTOR: number = 50;
-  private readonly SEGMENTS: number = 32;
-  private readonly RADIUS_FACTOR: number = 2;
-  private readonly COLOR_MASK: number = 0xFFFFFF;
+  public createCube(obj: IObject.ICube): THREE.Mesh {
+    const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(obj.sideLenght, obj.sideLenght, obj.sideLenght);
+    const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: obj.color });
+    const cube: THREE.Mesh = new THREE.Mesh(geometry, material);
+    cube.position = new THREE.Vector3(obj.position[Coordinate.X], obj.position[Coordinate.Y], obj.position[Coordinate.Z]);
+    cube.rotateX(obj.rotation[Coordinate.X]);
+    cube.rotateY(obj.rotation[Coordinate.Y]);
+    cube.rotateZ(obj.rotation[Coordinate.Z]);
 
-  private sizeGenerator(): number {
-    const percentFactor: number = 100;
-
-    return (
-      Math.floor(Math.random() * (this.MAX_SIZE_FACTOR - this.MIN_SIZE_FACTOR + 1) + this.MIN_SIZE_FACTOR) /
-      percentFactor * this.BASE_SIZE
-    );
+    return cube;
   }
 
-  public createCube(): THREE.Mesh {
-    const cubeSide: number = this.sizeGenerator();
-    const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(cubeSide, cubeSide, cubeSide);
-    const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: (Math.random() * this.COLOR_MASK) });
+  public createSphere(obj: IObject.ISphere): THREE.Mesh {
+    const geometry: THREE.SphereGeometry = new THREE.SphereGeometry(obj.radius, obj.widthSegments, obj.heightSegments);
+    const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: obj.color });
+    const sphere: THREE.Mesh = new THREE.Mesh(geometry, material);
+    sphere.position = new THREE.Vector3(obj.position[Coordinate.X], obj.position[Coordinate.Y], obj.position[Coordinate.Z]);
+    sphere.rotateX(obj.rotation[Coordinate.X]);
+    sphere.rotateY(obj.rotation[Coordinate.Y]);
+    sphere.rotateZ(obj.rotation[Coordinate.Z]);
 
-    return new THREE.Mesh(geometry, material);
+    return sphere;
   }
 
-  public createSphere(): THREE.Mesh {
-    const sphereRadius: number = this.sizeGenerator() / this.RADIUS_FACTOR;
-    const geometry: THREE.SphereGeometry = new THREE.SphereGeometry(sphereRadius, this.SEGMENTS, this.SEGMENTS);
-    const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: (Math.random() * this.COLOR_MASK) });
+  public createCone(obj: IObject.ICone): THREE.Mesh {
+    const geometry: THREE.ConeGeometry = new THREE.ConeGeometry(obj.radius, obj.height, obj.radialSegment);
+    const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: obj.color });
+    const cone: THREE.Mesh = new THREE.Mesh(geometry, material);
+    cone.position = new THREE.Vector3(obj.position[Coordinate.X], obj.position[Coordinate.Y], obj.position[Coordinate.Z]);
+    cone.rotateX(obj.rotation[Coordinate.X]);
+    cone.rotateY(obj.rotation[Coordinate.Y]);
+    cone.rotateZ(obj.rotation[Coordinate.Z]);
 
-    return new THREE.Mesh(geometry, material);
+    return cone;
   }
 
-  public createCone(): THREE.Mesh {
-    const coneSize: number = this.sizeGenerator();
-    const geometry: THREE.ConeGeometry = new THREE.ConeGeometry(
-      coneSize / this.RADIUS_FACTOR,
-      coneSize,
-      this.SEGMENTS,
-    );
-    const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: (Math.random() * this.COLOR_MASK) });
-
-    return new THREE.Mesh(geometry, material);
-  }
-
-  public createCylinder(): THREE.Mesh {
-    const cylinderSize: number = this.sizeGenerator();
+  public createCylinder(obj: IObject.ICylinder): THREE.Mesh {
     const geometry: THREE.CylinderGeometry = new THREE.CylinderGeometry(
-      cylinderSize / this.RADIUS_FACTOR,
-      cylinderSize / this.RADIUS_FACTOR,
-      cylinderSize,
-      this.SEGMENTS,
+      obj.topRadius,
+      obj.botRadius,
+      obj.height,
+      obj.radiusSegment,
     );
-    const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: (Math.random() * this.COLOR_MASK) });
+    const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: obj.color });
+    const cylinder: THREE.Mesh = new THREE.Mesh(geometry, material);
+    cylinder.position = new THREE.Vector3(obj.position[Coordinate.X], obj.position[Coordinate.Y], obj.position[Coordinate.Z]);
+    cylinder.rotateX(obj.rotation[Coordinate.X]);
+    cylinder.rotateY(obj.rotation[Coordinate.Y]);
+    cylinder.rotateZ(obj.rotation[Coordinate.Z]);
 
-    return new THREE.Mesh(geometry, material);
+    return cylinder;
   }
 
-  public createPyramid(): THREE.Mesh {
-    const pyramidSize: number = this.sizeGenerator();
-    const baseSides: number = 3;
+  public createPyramid(obj: IObject.IPyramid): THREE.Mesh {
     const geometry: THREE.CylinderGeometry = new THREE.CylinderGeometry(
-      0,
-      pyramidSize / this.RADIUS_FACTOR,
-      pyramidSize,
-      baseSides,
-      1,
+      obj.topRadius,
+      obj.botRadius,
+      obj.height,
+      obj.radiusSegment,
+      obj.heightSegment,
     );
-    const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: (Math.random() * this.COLOR_MASK) });
+    const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: obj.color });
+    const pyramid: THREE.Mesh = new THREE.Mesh(geometry, material);
+    pyramid.position = new THREE.Vector3(obj.position[Coordinate.X], obj.position[Coordinate.Y], obj.position[Coordinate.Z]);
+    pyramid.rotateX(obj.rotation[Coordinate.X]);
+    pyramid.rotateY(obj.rotation[Coordinate.Y]);
+    pyramid.rotateZ(obj.rotation[Coordinate.Z]);
 
-    return new THREE.Mesh(geometry, material);
+    return pyramid;
   }
 }
