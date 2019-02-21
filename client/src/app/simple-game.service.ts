3572f1a1d4bd06685a35a39a69b524ea56cb3416
Philippe Rivest <technophil98@gmail.com>
2019-02-21 10:13:29 -0500

    [D] Implemented pixel copy logic

--- client/src/app/simple-game.service.ts
+++ client/src/app/simple-game.service.ts
@@ -1,10 +1,55 @@
-import { Injectable } from "@angular/core";
+import {Injectable} from "@angular/core";
+import Axios, {AxiosResponse} from "axios";
+import {IDiffValidatorControllerRequest} from "../../../common/communication/requests/diff-validator-controller.request";
+import {DIFFERENCE_CLUSTER_POINTS_INDEX, DifferenceCluster} from "../../../common/model/game/simple-game";
+import {IPoint} from "../../../common/model/point";
+
+export const IMAGE_DATA_PIXEL_SIZE: number = 3;
 
 @Injectable({
-  providedIn: "root",
-})
+              providedIn: "root",
+            })
 export class SimpleGameService {
+  public constructor() {
+  }
 
-  public constructor() { }
+  private _gameName: string;
 
+  public set gameName(value: string) {
+    this._gameName = value;
+  }
+
+  public static getImageDataForDifferenceCluster(
+    differenceCluster: DifferenceCluster,
+    originalImageData: ImageData): ImageData {
+
+    const appendedImageData: ImageData = new ImageData(originalImageData.width, originalImageData.height);
+
+    for (const point of differenceCluster[DIFFERENCE_CLUSTER_POINTS_INDEX]) {
+      const pixelStartIndex: number = originalImageData.width * point.y + point.x;
+      const pixelData: Uint8ClampedArray = originalImageData.data.slice(pixelStartIndex, pixelStartIndex + IMAGE_DATA_PIXEL_SIZE);
+
+      for (let i: number = 0; i < pixelData.length; i++) {
+        appendedImageData.data[pixelStartIndex + i] = pixelData[i];
+      }
+    }
+
+    return appendedImageData;
+  }
+
+  public async validateDifferenceAtPoint(point: IPoint): Promise<DifferenceCluster> {
+    return Axios.get<DifferenceCluster>(
+      "http://localhost:3000/api/image-diff",
+      {
+        data: {
+          coord: point,
+          gameName: this._gameName,
+        } as IDiffValidatorControllerRequest,
+      })
+      .then((value: AxiosResponse<DifferenceCluster>) => {
+        const differenceCluster: DifferenceCluster = value.data;
+        // TODO: encountered differences
+        return differenceCluster;
+      });
+  }
 }
--- client/src/app/simple-game/simple-game-container/simple-game-container.component.ts
+++ client/src/app/simple-game/simple-game-container/simple-game-container.component.ts
@@ -1,31 +1,51 @@
 import {Component, Input, OnInit, ViewChild} from "@angular/core";
+import {DifferenceCluster} from "../../../../../common/model/game/simple-game";
 import {IPoint} from "../../../../../common/model/point";
 import {SimpleGameService} from "../../simple-game.service";
 import {SimpleGameCanvasComponent} from "../simple-game-canvas/simple-game-canvas.component";
 
 @Component({
-  selector: "app-simple-game-container",
-  templateUrl: "./simple-game-container.component.html",
-  styleUrls: ["./simple-game-container.component.css"],
-})
+             selector: "app-simple-game-container",
+             templateUrl: "./simple-game-container.component.html",
+             styleUrls: ["./simple-game-container.component.css"],
+           })
 export class SimpleGameContainerComponent implements OnInit {
 
+  @Input() public gameName: string;
   @Input() public originalImage: string;
   @Input() public modifiedImage: string;
 
   @ViewChild("originalImageComponent") private originalImageComponent: SimpleGameCanvasComponent;
   @ViewChild("modifiedImageComponent") private modifiedImageComponent: SimpleGameCanvasComponent;
 
-  public constructor(private simpleGameService: SimpleGameService) { }
+  public constructor(private simpleGameService: SimpleGameService) {
+  }
 
-  public ngOnInit() {
+  public ngOnInit(): void {
+    this.simpleGameService.gameName = this.gameName;
+  }
+
+  public onCanvasClick(clickEvent: IPoint): void {
+    this.simpleGameService.validateDifferenceAtPoint(clickEvent)
+      .then((differenceCluster: DifferenceCluster) => {
+        this.copyPixelsForCluster(differenceCluster);
+      })
+      .catch((error: Error) => {
+        // TODO: Handle difference not found
+        this.playSound();
+      });
+  }
+
+  public playSound() {
 
   }
 
-  public onCanvasClick(clickEvent: IPoint) {
+  private copyPixelsForCluster(differenceCluster: DifferenceCluster): void {
+    const imageData: ImageData = SimpleGameService.getImageDataForDifferenceCluster(
+      differenceCluster,
+      this.originalImageComponent.canvasImageData,
+    );
 
-    // TODO: Call service
-    this.modifiedImageComponent.appendToCanvas(appendedImageData);
+    this.modifiedImageComponent.appendToCanvas(imageData);
   }
-
 }
