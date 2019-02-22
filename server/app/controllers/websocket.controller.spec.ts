2228eaa9deda711ca1d2c83a605816ed9a3b74ca
ItsTehStory <philippe.ballandras@hotmail.com>
2019-02-21 22:50:35 -0500

    [Test] Added a test file for WebsocketControler

---
+++ server/app/controllers/websocket.controller.spec.ts
@@ -0,0 +1,18 @@
+// tslint:disable:typedef
+import { expect } from "chai";
+// import { Application } from "../app";
+// import { container } from "../inversify.config";
+// import types from "../types";
+
+describe("username controller", () => {
+    // let app: Express.Application;
+
+    beforeEach(() => {
+        // container.rebind(types.DummyWebsocketActionService).toConstantValue(mockedUsernameService);
+        // app = container.get<Application>(types.Application).app;
+    });
+
+    it ("should test", () => {
+        expect(true).to.be.true;
+    });
+});
