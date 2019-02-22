72771973abdc129cc3170a148e504cdc777c13d5
Philippe Ballandras <philippe.ballandras@hotmail.com>
2019-02-22 12:05:48 -0500

    [Dev] Implemented client websocket service

--- client/src/app/socket.service.ts
+++ client/src/app/socket.service.ts
@@ -1,12 +1,37 @@
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { connect } from "socket.io-client";
+import { environment } from "src/environments/environment";
+import { WebsocketMessage } from "../../../common/communication/messages/message";
+import { SocketEvent } from "../../../common/communication/socket-events";
 
 @Injectable()
 export class SocketService {
 
-    private readonly BASE_URL: string = "http://localhost:3000/";
+    private readonly BASE_URL: string = environment.production ? document.baseURI : "http://localhost:3000/";
+    private socket: SocketIOClient.Socket;
 
-    public openSocket(): SocketIOClient.Socket {
+    public constructor () {
+        this.socket = this.openSocket();
+    }
+
+    private openSocket(): SocketIOClient.Socket {
         return connect(this.BASE_URL);
+    }
+
+    public send(event: SocketEvent, message: WebsocketMessage): void {
+        this.socket.emit(event, message);
+    }
+
+    public onMessage(): Observable<WebsocketMessage> {
+        return new Observable<WebsocketMessage>((observer) => {
+            this.socket.on("message", (data: WebsocketMessage) => observer.next(data));
+        });
+    }
+
+    public onEvent(event: SocketEvent): Observable<WebsocketMessage> {
+        return new Observable<WebsocketMessage>((observer) => {
+            this.socket.on(event, (message: WebsocketMessage) => observer.next(message));
+        });
     }
 }
