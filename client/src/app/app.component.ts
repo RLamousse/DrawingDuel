147b2550a5613a1db3e11cf2515a01ee1cd66fc8
Philippe Ballandras <philippe.ballandras@hotmail.com>
2019-02-22 12:07:13 -0500

    [Dev] Reset of app component to orininal version

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,34 +1,9 @@
-import { Component, OnInit } from "@angular/core";
-import { SocketEvent } from "../../../common/communication/socket-events";
-import { SocketService } from "./socket.service";
-
+import { Component } from "@angular/core";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   public readonly title: string = "LOG2990";
-
-  private socket: SocketIOClient.Socket;
-
-  public constructor(private socketService: SocketService) { }
-
-  public ngOnInit(): void {
-    this.connectSocket();
-  }
-
-  public connectSocket(): void {
-    this.socket = this.socketService.openSocket();
-    this.socket.on("connect", (something: string) => {
-      console.log(something);
-      this.socket.emit(SocketEvent.DUMMY, "Yo max");
-      this.socket.on(SocketEvent.WELCOME, (data: Object) => {
-        console.log(data);
-      });
-      this.socket.on(SocketEvent.DUMMY, (message: string) => {
-        console.log(message);
-      });
-    });
-  }
 }
