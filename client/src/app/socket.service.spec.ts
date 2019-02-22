feaf220943f25ba9253cd6652e0ac20d7df141ac
Phil <philippe.ballandras@hotmail.com>
2019-02-22 09:12:46 -0500

    [Test] Added test file for socket.service

---
+++ client/src/app/socket.service.spec.ts
@@ -0,0 +1,10 @@
+import { BITMAP_HEADER_24BPP, HEADER_SIZE_BYTES } from "../../../common/image/bitmap/bitmap-utils";
+import FakeControl from "./fakeControl";
+import FileValidator from "./file.validator";
+
+describe("FileValidator", () => {
+
+  it("should be true", () => {
+    expect(true).toBeTruthy();
+  });
+});
