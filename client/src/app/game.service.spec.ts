e5da7d193d36cf524f1500315699d471753d7a6c
Ophion <li.wenbo555@hotmail.com>
2019-02-24 15:34:15 -0500

    [fix] Tslint fix

--- client/src/app/game.service.spec.ts
+++ client/src/app/game.service.spec.ts
@@ -1,10 +1,10 @@
 import { HttpClientModule } from "@angular/common/http";
 import { async, TestBed } from "@angular/core/testing";
+import * as THREE from "three";
 import {IFreeGame} from "../../../common/model/game/free-game";
 import {IGame} from "../../../common/model/game/game";
 import {ISimpleGame} from "../../../common/model/game/simple-game";
 import { GameService } from "./game.service";
-import * as THREE from "three";
 
 describe("GameService", () => {
   let serviceGame: GameService;
