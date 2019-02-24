7660f36280cd4757ac168e78010321132284ace0
Philippe Rivest <technophil98@gmail.com>
2019-02-24 18:23:56 -0500

    [QA] Lint server

--- server/app/controllers/diff-validator.controller.ts
+++ server/app/controllers/diff-validator.controller.ts
@@ -2,7 +2,7 @@ import {NextFunction, Request, Response, Router} from "express";
 import * as Httpstatus from "http-status-codes";
 import {inject, injectable} from "inversify";
 import {IDiffValidatorControllerResponse} from "../../../common/communication/responses/diff-validator-controller.response";
-import {DIFFERENCE_CLUSTER_ID_INDEX, DIFFERENCE_CLUSTER_POINTS_INDEX, DifferenceCluster} from "../../../common/model/game/simple-game";
+import {DifferenceCluster, DIFFERENCE_CLUSTER_ID_INDEX, DIFFERENCE_CLUSTER_POINTS_INDEX} from "../../../common/model/game/simple-game";
 import {DiffValidatorService, NO_DIFFERENCE_AT_POINT_ERROR_MESSAGE} from "../services/diff-validator.service";
 import Types from "../types";
 import {assertParamsOfRequest, executePromiseSafely} from "./controller-utils";
