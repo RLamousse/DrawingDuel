019d2e4f97525824189be05c7a5aa4ac2a85f8e3
Philippe Ballandras <philippe.ballandras@hotmail.com>
2019-02-25 13:45:39 -0500

    [Lint] removed anys, added typedef from diff-check socket service tests

--- server/app/services/websocket/diff-check-websocket-action.service.spec.ts
+++ server/app/services/websocket/diff-check-websocket-action.service.spec.ts
@@ -1,7 +1,5 @@
 import Axios from "axios";
 import MockAdapter from "axios-mock-adapter";
-// tslint:disable-next-line:no-duplicate-imports Weird interaction between singletons and interface (olivier st-o approved)
-// import AxiosAdapter from "axios-mock-adapter";
 import {expect} from "chai";
 import { EventEmitter } from "events";
 import * as HttpStatus from "http-status-codes";
@@ -15,8 +13,7 @@ import { DiffCheckWebsocketActionService } from "./diff-check-websocket-action.s
 class FakeSocket extends EventEmitter {
     public messageIntercepted: WebsocketMessage;
     public resolver: () => void;
-    // tslint:disable-next-line:no-any
-    public emit (event: string | symbol, ...args: any[]): boolean {
+    public emit (event: string | symbol, ...args: WebsocketMessage[]): boolean {
         this.messageIntercepted = args[0];
         this.resolver();
 
@@ -24,9 +21,9 @@ class FakeSocket extends EventEmitter {
     }
 
     public async waitForEmit (): Promise<void> {
-        return new Promise<void> ((resolve) => {
+        return new Promise<void> ((resolve: () => void) => {
             this.resolver = resolve;
-        })
+        });
     }
 }
 
@@ -39,23 +36,23 @@ const fakeMessage: WebsocketMessage<IDiffValidatorControllerRequest> = {
     title: SocketEvent.DUMMY,
 };
 
-const socketMessageSuccess = {
+const socketMessageSuccess: WebsocketMessage<IDiffValidatorControllerResponse> = {
     title: SocketEvent.CHECK_DIFFERENCE,
     body: {
         differenceClusterCoords: [],
         differenceClusterId: 0,
         validDifference: true,
     },
-}
+};
 
-const socketMessageFail = {
+const socketMessageFail: WebsocketMessage<IDiffValidatorControllerResponse> = {
     title: SocketEvent.CHECK_DIFFERENCE,
     body: {
         differenceClusterCoords: [],
         differenceClusterId: 0,
         validDifference: true,
     },
-}
+};
 
 describe("A websocket service that bridges websocket and DiffValidatorController", () => {
 
@@ -72,7 +69,6 @@ describe("A websocket service that bridges websocket and DiffValidatorController
         diffChecker.execute(fakeMessage, fakeSocket as unknown as io.Socket);
 
         return fakeSocket.waitForEmit().then(() => {
-            // tslint:disable-next-line:no-any
             const val: boolean = (fakeSocket.messageIntercepted as WebsocketMessage<IDiffValidatorControllerResponse>).body.validDifference;
 
             return expect(val).to.be.false;
@@ -85,8 +81,8 @@ describe("A websocket service that bridges websocket and DiffValidatorController
         diffChecker.execute(fakeMessage, fakeSocket as unknown as io.Socket);
 
         return fakeSocket.waitForEmit().then(() => {
-            // tslint:disable-next-line:no-any
-            const val: boolean = (fakeSocket.messageIntercepted.body as any).body.validDifference;
+            const val: boolean = (fakeSocket.messageIntercepted.body as WebsocketMessage<IDiffValidatorControllerResponse>)
+                                 .body.validDifference;
 
             return expect(val).to.be.true;
         });
@@ -98,43 +94,9 @@ describe("A websocket service that bridges websocket and DiffValidatorController
         diffChecker.execute(fakeMessage, fakeSocket as unknown as io.Socket);
 
         return fakeSocket.waitForEmit().then(() => {
-            // tslint:disable-next-line:no-any
             const val: boolean = (fakeSocket.messageIntercepted as WebsocketMessage<IDiffValidatorControllerResponse>).body.validDifference;
 
             return expect(val).to.be.false;
         });
     });
-    // it("should throw if the point is out of bounds (y < 0)", async () => {
-    //     return diffValidatorService.getDifferenceCluster("game", {x: 0, y: -1})
-    //         .catch((reason: Error) => {
-    //             expect(reason.message).to.equal(INVALID_POINT_ERROR_MESSAGE);
-    //         });
-    // });
-    // it("should throw if the specified gameName is not valid", async () => {
-    //     axiosMock.onGet("http://localhost:3000/api/data-base/games/simple/notAValidGame")
-    //         .reply(HttpStatus.NOT_FOUND, {message: NON_EXISTING_GAME_ERROR_MESSAGE});
-
-    //     return diffValidatorService.getDifferenceCluster("notAValidGame", ORIGIN)
-    //         .catch((reason: Error) => {
-    //             expect(reason.message).to.equal(NON_EXISTING_GAME_ERROR_MESSAGE);
-    //         });
-    // });
-    // it("should return an empty list when the point is not part of a difference group", async () => {
-    //     axiosMock.onGet("http://localhost:3000/api/data-base/games/simple/game")
-    //         .reply(HttpStatus.OK, mockedSimpleGame);
-
-    //     return diffValidatorService.getDifferenceCluster("game", {x: 42, y: 42})
-    //         .catch((error: Error) => {
-    //             expect(error.message).to.eql(NO_DIFFERENCE_AT_POINT_ERROR_MESSAGE);
-    //         });
-    // });
-    // it("should return a difference group", async () => {
-    //     axiosMock.onGet("http://localhost:3000/api/data-base/games/simple/game")
-    //         .reply(HttpStatus.OK, mockedSimpleGame);
-
-    //     return diffValidatorService.getDifferenceCluster("game", {x: 0, y: 0})
-    //         .then((value: DifferenceCluster) => {
-    //             return expect(value).to.eql(mockedSimpleGame.diffData[0]);
-    //         });
-    // });
 });
