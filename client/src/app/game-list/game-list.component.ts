fdb77199f77ed87646d7ebfc037cad17fea0ff47
Robin Canton Lamousse <robin.canton-lamousse@polymtl.ca>
2019-02-25 21:05:47 -0500

    [Fix] import debug problem

--- client/src/app/game-list/game-list.component.ts
+++ client/src/app/game-list/game-list.component.ts
@@ -5,7 +5,6 @@ import { IFreeGame } from "../../../../common/model/game/free-game";
 import { ISimpleGame } from "../../../../common/model/game/simple-game";
 import { IScene } from "../../../scene-interface";
 import { GameService } from "../game.service";
-import { MOCKED_SIMPLE_GAMES } from "../mockGames";
 import { FreeGameCreatorService } from "../scene-creator/FreeGameCreator/free-game-creator.service";
 import { FreeGamePhotoService } from "../scene-creator/free-game-photo-service/free-game-photo.service";
 
