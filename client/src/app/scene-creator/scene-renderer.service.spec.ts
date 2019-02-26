7e7f384c8fa15de6ef1544eb24818af5149d36d2
Anthony Potvin <DarkHoTs66@hotmail.com>
2019-02-26 16:14:02 -0500

    [Dev] deleted onResize method

--- client/src/app/scene-creator/scene-renderer.service.spec.ts
+++ client/src/app/scene-creator/scene-renderer.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from "@angular/core/testing";
-//import * as THREE from "three";
+import * as THREE from "three";
 import { SceneRendererService } from "./scene-renderer.service";
 
 describe("SceneRendererService", () => {
@@ -12,5 +12,14 @@ describe("SceneRendererService", () => {
   it("should create", () => {
     const service: SceneRendererService = new SceneRendererService();
     expect(service).toBeDefined();
+  });
+
+  // Test loadScenes
+  it("should throw an error if loadScenes is called before init(...)", () => {
+    const service: SceneRendererService = TestBed.get(SceneRendererService);
+    const original: THREE.Scene = new THREE.Scene();
+    const modified: THREE.Scene = new THREE.Scene();
+
+    expect(() => service.loadScenes(original, modified)).toThrowError("La composante n'a pas ete initialise!");
   });
 });
