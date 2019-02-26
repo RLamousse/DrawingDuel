0d7835ba9b3ebe12802377708eefb0a7e6222e8f
Anthony Potvin <DarkHoTs66@hotmail.com>
2019-02-26 16:30:28 -0500

    [Test] Improved test in sceneRendererService, coverage OK, no lint errors

--- client/src/app/scene-creator/scene-renderer.service.spec.ts
+++ client/src/app/scene-creator/scene-renderer.service.spec.ts
@@ -22,4 +22,31 @@ describe("SceneRendererService", () => {
 
     expect(() => service.loadScenes(original, modified)).toThrowError("La composante n'a pas ete initialise!");
   });
+
+  it("should asign scenes at first call", () => {
+    const service: SceneRendererService = TestBed.get(SceneRendererService);
+    const original: THREE.Scene = new THREE.Scene();
+    const modified: THREE.Scene = new THREE.Scene();
+    const oriCont: HTMLDivElement = (document.createElement("div")) as HTMLDivElement;
+    const modCont: HTMLDivElement = (document.createElement("div")) as HTMLDivElement;
+    service.init(oriCont, modCont);
+    service.loadScenes(original, modified);
+    expect(service.scene).toBe(original);
+    expect(service.modifiedScene).toBe(modified);
+  });
+
+  it("should reasign the new scenes at second call", () => {
+    const service: SceneRendererService = TestBed.get(SceneRendererService);
+    const original1: THREE.Scene = new THREE.Scene();
+    const modified1: THREE.Scene = new THREE.Scene();
+    const original2: THREE.Scene = new THREE.Scene();
+    const modified2: THREE.Scene = new THREE.Scene();
+    const oriCont: HTMLDivElement = (document.createElement("div")) as HTMLDivElement;
+    const modCont: HTMLDivElement = (document.createElement("div")) as HTMLDivElement;
+    service.init(oriCont, modCont);
+    service.loadScenes(original1, modified1);
+    service.loadScenes(original2, modified2);
+    expect(service.scene).toBe(original2);
+    expect(service.modifiedScene).toBe(modified2);
+  });
 });
