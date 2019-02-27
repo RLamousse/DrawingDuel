6491e34b97eca1e4d811f8a2749fed0f7a751acd
Philippe Rivest <technophil98@gmail.com>
2019-02-26 20:37:47 -0500

    [B] Fixed play-view.component tests

--- client/src/app/play-view/play-view.component.spec.ts
+++ client/src/app/play-view/play-view.component.spec.ts
@@ -1,9 +1,10 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { ActivatedRoute, Router} from "@angular/router";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {ActivatedRoute, Router} from "@angular/router";
 import {CompteurDiffComponent} from "../compteur-diff/compteur-diff.component";
 import {SimpleGameCanvasComponent} from "../simple-game/simple-game-canvas/simple-game-canvas.component";
 import {SimpleGameContainerComponent} from "../simple-game/simple-game-container/simple-game-container.component";
-import { PlayViewComponent } from "./play-view.component";
+import {TimerComponent} from "../timer/timer.component";
+import {PlayViewComponent} from "./play-view.component";
 
 describe("PlayViewComponent", () => {
   let component: PlayViewComponent;
@@ -12,10 +13,12 @@ describe("PlayViewComponent", () => {
   beforeEach((done) => {
     TestBed.configureTestingModule(
       {
-        declarations: [PlayViewComponent,
-                       CompteurDiffComponent,
-                       SimpleGameContainerComponent,
-                       SimpleGameCanvasComponent,
+        declarations: [
+          PlayViewComponent,
+          CompteurDiffComponent,
+          SimpleGameContainerComponent,
+          SimpleGameCanvasComponent,
+          TimerComponent,
         ],
         providers: [
         { provide: Router, useClass: class { public navigate: jasmine.Spy = jasmine.createSpy("navigate"); }, },
