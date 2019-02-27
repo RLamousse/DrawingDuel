bf229fa9ba67b851a0808230de713d8d53f68137
Philippe Ballandras <philippe.ballandras@hotmail.com>
2019-02-26 21:56:31 -0500

    [Test] 100% coverage on form-post-service

--- client/src/app/form-post.service.spec.ts
+++ client/src/app/form-post.service.spec.ts
@@ -1,21 +1,76 @@
-import { TestBed } from "@angular/core/testing";
-
-import { HttpClientModule } from "@angular/common/http";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from "@angular/common/http/testing";
+import { getTestBed, TestBed } from "@angular/core/testing";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormPostService } from "./form-post.service";
 
 describe("FormPostService", () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
+  let injector: TestBed;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
         BrowserModule,
-        HttpClientModule,
         HttpClientTestingModule,
       ],
-  }));
+    });
+    injector = getTestBed();
+    httpMock = injector.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
 
   it("should be created", () => {
     const service: FormPostService = TestBed.get(FormPostService);
     expect(service).toBeTruthy();
+  });
+
+  it("should send a not undefined error message with Backend Error", async (done) => {
+    const service: FormPostService = TestBed.get(FormPostService);
+    // tslint:disable-next-line:typedef
+    const errorSent = {
+      message: "Test Message",
+      type: "Testing",
+      error: {
+        message: "Test Message",
+        name: "Max",
+      },
+
+    };
+    service.submitForm("", {}).subscribe(
+      (data) => {
+        //
+      },
+      (error: Error) => {
+        expect(error.name).toEqual("Backend Error");
+        expect(error.message).toEqual("Test Message");
+        done();
+      },
+    );
+    const req: TestRequest = httpMock.expectOne("http://localhost:3000/");
+    req.error(errorSent as ErrorEvent);
+    expect(req.request.method).toBe("POST");
+  });
+
+  it("should send a not undefined error message with Network Error", async (done) => {
+    const service: FormPostService = TestBed.get(FormPostService);
+    // tslint:disable-next-line:typedef
+    const errorSent = new ErrorEvent("Maxime");
+
+    service.submitForm("", {}).subscribe(
+      (data) => {
+        //
+      },
+      (error: Error) => {
+        expect(error.name).toEqual("Network Error");
+        expect(error.message).toEqual(FormPostService.NETWORK_ERROR_MESSAGE);
+        done();
+      },
+    );
+    const req: TestRequest = httpMock.expectOne("http://localhost:3000/");
+    req.error(errorSent as ErrorEvent);
+    expect(req.request.method).toBe("POST");
   });
 });
--- client/src/app/form-post.service.ts
+++ client/src/app/form-post.service.ts
@@ -9,10 +9,12 @@ import { environment } from "src/environments/environment";
   providedIn: "root",
 })
 export class FormPostService {
+  public constructor(private http: HttpClient) { }
+  public static readonly NETWORK_ERROR_MESSAGE: string = "La requête n'a pas pu être acheminée depuis le client";
+  public static readonly BACKEND_ERROR_MESSAGE: string = "La requête n'a pas pu être acheminée depuis le client";
 
   // Since we are in dev we can't use the base URL of the document, hence the hardcode
   private readonly BASE_URL: string = environment.production ? document.baseURI : "http://localhost:3000/";
-  public constructor(private http: HttpClient) { }
 
   public submitForm(route: string, body: FormData | {}): Observable<Object> {
 
@@ -28,7 +30,7 @@ export class FormPostService {
       console.error("An error occurred:", error.error.message);
       err = {
         name: "Network Error",
-        message: "La requête n'a pas pu être acheminée depuis le client",
+        message: FormPostService.NETWORK_ERROR_MESSAGE,
       };
     } else {
       // The backend returned an unsuccessful response code.
@@ -38,7 +40,7 @@ export class FormPostService {
         `body was: ${error.error}`);
       err = {
         name: "Backend Error",
-        message: error.error.message ? error.error.message : "La requête n'a pas pu être traitée par le serveur",
+        message: error.error.message ? error.error.message : FormPostService.BACKEND_ERROR_MESSAGE,
       };
     }
     // return an observable with a user-facing error message
