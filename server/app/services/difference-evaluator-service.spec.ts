35e65408d1bcbfa387cb36273c5a86e0e789c72b
Ophion <li.wenbo555@hotmail.com>
2019-02-26 21:06:35 -0500

    [QA] justified ts-ignore

--- server/app/services/difference-evaluator-service.spec.ts
+++ server/app/services/difference-evaluator-service.spec.ts
@@ -54,24 +54,28 @@ describe("Difference evaluator service", () => {
         });
 
         it("Should throw a format error if the input is not an array(string)", () => {
+            // we need to put the wrong format of input to get an error
             // @ts-ignore
             expect(() => DIFFERENCE_EVALUATOR_SERVICE.getSimpleNDifferences("abs"))
                 .to.throw(ARGUMENT_ERROR_MESSAGE);
         });
 
         it("Should throw a format error if the input is not an array(undefined)", () => {
+            // we need to put the wrong format of input to get an error
             // @ts-ignore
             expect(() => DIFFERENCE_EVALUATOR_SERVICE.getSimpleNDifferences(null))
                 .to.throw(ARGUMENT_ERROR_MESSAGE);
         });
 
         it("Should throw a format error if the input is not an array of numbers(strings)", () => {
+            // we need to put the wrong format of input to get an error
             // @ts-ignore
             expect(() => DIFFERENCE_EVALUATOR_SERVICE.getSimpleNDifferences([[""]]))
                 .to.throw(ARGUMENT_ERROR_MESSAGE);
         });
 
         it("Should throw a format error if the input is not an array of numbers(undefined)", () => {
+            // we need to put the wrong format of input to get an error
             // @ts-ignore
             expect(() => DIFFERENCE_EVALUATOR_SERVICE.getSimpleNDifferences([[null]]))
                 .to.throw(ARGUMENT_ERROR_MESSAGE);
