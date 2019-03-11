c67db71dd5ceaffbbff13db0604455b4347d0c5a
Philippe Ballandras <philippe.ballandras@hotmail.com>
2019-03-11 13:04:49 -0400

    [Dev] Definition of a ChatMessage as an interface

--- common/communication/messages/message.ts
+++ common/communication/messages/message.ts
@@ -10,6 +10,21 @@ export interface WebsocketMessage {
     body: Object;
 }
 
+export enum ChatMessagePosition {
+    FIRST = "première", SECOND = "deuxième" , THIRD = "troisième"
+}
+
+export enum ChatMessagePlayerCount {
+    SOLO = "solo", MULTI = "un contre un"
+}
+export interface ChatMessage {
+    timestamp: Date;
+    playerName: string;
+    gameName: string;
+    position: ChatMessagePosition;
+    playerCount: ChatMessagePlayerCount;
+}
+
 export function isAWebsocketMessage (object: any) {
     return (object.title !== undefined && object.body !== undefined);
 }
