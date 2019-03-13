c97d9cc89e598e57425a8f03522d70e467b121cb
Robin Canton Lamousse <robin.canton-lamousse@polymtl.ca>
2019-03-13 12:15:58 -0400

    [dev+test] scoreTable service works

--- server/app/services/score-table.service.spec.ts
+++ server/app/services/score-table.service.spec.ts
@@ -2,7 +2,7 @@ import {expect} from "chai";
 import {IRecordTime} from "../../../common/model/game/record-time";
 import {ScoreTableService} from "./score-table.service";
 
-describe("Object3DCreatorService", () => {
+describe("ScoreTableService", () => {
     let service: ScoreTableService;
 
     // @ts-ignore
@@ -11,6 +11,7 @@ describe("Object3DCreatorService", () => {
     const highTimeScoreBoy: IRecordTime = {name: "Tommy", time: 7};
     const middleTimeScoreBoy: IRecordTime = {name: "Phil", time: 3};
     const lowTimeScoreBoy: IRecordTime = {name: "Bob", time: 1};
+    const sameThanSecondScore: IRecordTime = {name: "Jane", time: 5};
     const initialScoreTable: IRecordTime[] = [{name: "Paul", time: 2},
                                               {name: "Jack", time: 5},
                                               {name: "Bill", time: 10}];
@@ -25,23 +26,21 @@ describe("Object3DCreatorService", () => {
     });
 
     it("should return the same table if the time inserted is too high", () => {
-        const table: IRecordTime[] = initialScoreTable;
+        const table: IRecordTime[] = JSON.parse(JSON.stringify(initialScoreTable));
         service.insertTime(table, veryHighTimeScoreBoy);
         expect(table).to.eql(initialScoreTable);
     });
 
     it("should return a new table with the highTimeScoreBoy at the third place if you insert it", () => {
-        const table: IRecordTime[] = initialScoreTable;
-        console.log(initialScoreTable);
+        const table: IRecordTime[] = JSON.parse(JSON.stringify(initialScoreTable));
         service.insertTime(table, highTimeScoreBoy);
-        console.log(table);
         expect(table[0]).to.eql(initialScoreTable[0]);
         expect(table[1]).to.eql(initialScoreTable[1]);
         expect(table[2]).to.eql(highTimeScoreBoy);
     });
 
     it("should return a new table with the middleTimeScoreBoy at the second place if you insert it", () => {
-        const table: IRecordTime[] = initialScoreTable;
+        const table: IRecordTime[] = JSON.parse(JSON.stringify(initialScoreTable));
         service.insertTime(table, middleTimeScoreBoy);
         expect(table[0]).to.eql(initialScoreTable[0]);
         expect(table[1]).to.eql(middleTimeScoreBoy);
@@ -49,10 +48,20 @@ describe("Object3DCreatorService", () => {
     });
 
     it("should return a new table with the lowTimeScoreBoy at the first place if you insert it", () => {
-        const table: IRecordTime[] = initialScoreTable;
+        const table: IRecordTime[] = JSON.parse(JSON.stringify(initialScoreTable));
         service.insertTime(table, lowTimeScoreBoy);
         expect(table[0]).to.eql(lowTimeScoreBoy);
         expect(table[1]).to.eql(initialScoreTable[0]);
         expect(table[2]).to.eql(initialScoreTable[1]);
+    });
+
+    it("should not replace if you insert already existing time", () => {
+        const table: IRecordTime[] = JSON.parse(JSON.stringify(initialScoreTable));
+        service.insertTime(table, sameThanSecondScore);
+        console.dir(initialScoreTable);
+        console.dir(table);
+        expect(table[0]).to.eql(initialScoreTable[0]);
+        expect(table[1]).to.eql(initialScoreTable[1]);
+        expect(table[2]).to.eql(sameThanSecondScore);
     });
 });
