22d0c0ea86eb00fcaf2125169c5371176bdeac7f
Robin Canton Lamousse <robin.canton-lamousse@polymtl.ca>
2019-03-13 12:30:46 -0400

    [test] more tests for scoreTableService

--- server/app/services/score-table.service.spec.ts
+++ server/app/services/score-table.service.spec.ts
@@ -3,37 +3,34 @@ import {IRecordTime} from "../../../common/model/game/record-time";
 import {ScoreTableService} from "./score-table.service";
 
 describe("ScoreTableService", () => {
-    let service: ScoreTableService;
-
     // @ts-ignore
     const emptyTime: IRecordTime = null;
     const veryHighTimeScoreBoy: IRecordTime = {name: "Rob", time: 55};
     const highTimeScoreBoy: IRecordTime = {name: "Tommy", time: 7};
     const middleTimeScoreBoy: IRecordTime = {name: "Phil", time: 3};
     const lowTimeScoreBoy: IRecordTime = {name: "Bob", time: 1};
+    const sameThanFirstScore: IRecordTime = {name: "Peter", time: 2};
     const sameThanSecondScore: IRecordTime = {name: "Jane", time: 5};
+    const sameThanThirdScore: IRecordTime = {name: "Anthony", time: 10};
     const initialScoreTable: IRecordTime[] = [{name: "Paul", time: 2},
                                               {name: "Jack", time: 5},
                                               {name: "Bill", time: 10}];
-    beforeEach(() => {
-        service = new ScoreTableService();
-    });
 
     // Test createCube
     it("should throw if null time inserted", () => {
         const table: IRecordTime[] = initialScoreTable;
-        expect(() => service.insertTime(table, emptyTime)).to.throw();
+        expect(() => ScoreTableService.insertTime(table, emptyTime)).to.throw();
     });
 
     it("should return the same table if the time inserted is too high", () => {
         const table: IRecordTime[] = JSON.parse(JSON.stringify(initialScoreTable));
-        service.insertTime(table, veryHighTimeScoreBoy);
+        ScoreTableService.insertTime(table, veryHighTimeScoreBoy);
         expect(table).to.eql(initialScoreTable);
     });
 
     it("should return a new table with the highTimeScoreBoy at the third place if you insert it", () => {
         const table: IRecordTime[] = JSON.parse(JSON.stringify(initialScoreTable));
-        service.insertTime(table, highTimeScoreBoy);
+        ScoreTableService.insertTime(table, highTimeScoreBoy);
         expect(table[0]).to.eql(initialScoreTable[0]);
         expect(table[1]).to.eql(initialScoreTable[1]);
         expect(table[2]).to.eql(highTimeScoreBoy);
@@ -41,7 +38,7 @@ describe("ScoreTableService", () => {
 
     it("should return a new table with the middleTimeScoreBoy at the second place if you insert it", () => {
         const table: IRecordTime[] = JSON.parse(JSON.stringify(initialScoreTable));
-        service.insertTime(table, middleTimeScoreBoy);
+        ScoreTableService.insertTime(table, middleTimeScoreBoy);
         expect(table[0]).to.eql(initialScoreTable[0]);
         expect(table[1]).to.eql(middleTimeScoreBoy);
         expect(table[2]).to.eql(initialScoreTable[1]);
@@ -49,19 +46,31 @@ describe("ScoreTableService", () => {
 
     it("should return a new table with the lowTimeScoreBoy at the first place if you insert it", () => {
         const table: IRecordTime[] = JSON.parse(JSON.stringify(initialScoreTable));
-        service.insertTime(table, lowTimeScoreBoy);
+        ScoreTableService.insertTime(table, lowTimeScoreBoy);
         expect(table[0]).to.eql(lowTimeScoreBoy);
         expect(table[1]).to.eql(initialScoreTable[0]);
         expect(table[2]).to.eql(initialScoreTable[1]);
     });
 
-    it("should not replace if you insert already existing time", () => {
+    it("should not replace if you insert same than first", () => {
         const table: IRecordTime[] = JSON.parse(JSON.stringify(initialScoreTable));
-        service.insertTime(table, sameThanSecondScore);
-        console.dir(initialScoreTable);
-        console.dir(table);
+        ScoreTableService.insertTime(table, sameThanFirstScore);
+        expect(table[0]).to.eql(initialScoreTable[0]);
+        expect(table[1]).to.eql(sameThanFirstScore);
+        expect(table[2]).to.eql(initialScoreTable[1]);
+    });
+
+    it("should not replace if you insert same than second", () => {
+        const table: IRecordTime[] = JSON.parse(JSON.stringify(initialScoreTable));
+        ScoreTableService.insertTime(table, sameThanSecondScore);
         expect(table[0]).to.eql(initialScoreTable[0]);
         expect(table[1]).to.eql(initialScoreTable[1]);
         expect(table[2]).to.eql(sameThanSecondScore);
+    });
+
+    it("should not replace if you insert same than third", () => {
+        const table: IRecordTime[] = JSON.parse(JSON.stringify(initialScoreTable));
+        ScoreTableService.insertTime(table, sameThanThirdScore);
+        expect(table).to.eql(initialScoreTable);
     });
 });
