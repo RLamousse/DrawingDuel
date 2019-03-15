3bddfdcfe6c12f5ff8a959a3fa3e15276d6c4d8e
Anthony Potvin <DarkHoTs66@hotmail.com>
2019-03-15 11:28:14 -0400

    [B] fixed bug where unable to get array of diffObj for 3d diff validator

--- server/app/services/diff-validator-3D.service.ts
+++ server/app/services/diff-validator-3D.service.ts
@@ -4,17 +4,19 @@ import "reflect-metadata";
 import {DB_FREE_GAME, SERVER_BASE_URL} from "../../../common/communication/routes";
 import {Object3DIsNotADifference} from "../../../common/errors/services.errors";
 import {Coordinate} from "../../../common/free-game-json-interface/FreeGameCreatorInterface/free-game-enum";
-import {IJson3DObject, IScenesDB} from "../../../common/free-game-json-interface/JSONInterface/IScenesJSON";
+import {
+    IJson3DObject,
+    IScenesDB,
+} from "../../../common/free-game-json-interface/JSONInterface/IScenesJSON";
+import {IFreeGame} from "../../../common/model/game/free-game";
 
 @injectable()
 export class DiffValidator3DService {
     public async getDifferentObjects(gameName: string, center: number[]): Promise<IJson3DObject> {
 
-       return this.getGame(gameName).then((value: IScenesDB) => {
-            const diffObjs: IJson3DObject[] = value.differentObjects;
-            console.log(value);
-            console.log(value.differentObjects[0]);
-            for (const obj of diffObjs) {
+       return this.getGame(gameName).then((value: IFreeGame) => {
+            const diffObjs: IScenesDB = (value.scenes as IScenesDB);
+            for (const obj of diffObjs.differentObjects) {
                 if (obj.position[Coordinate.X] === center[Coordinate.X] &&
                     obj.position[Coordinate.Y] === center[Coordinate.Y] &&
                     obj.position[Coordinate.Z] === center[Coordinate.Z]
@@ -26,9 +28,9 @@ export class DiffValidator3DService {
         });
     }
 
-    private async getGame(gameName: string): Promise<IScenesDB> {
-        return Axios.get<IScenesDB>(SERVER_BASE_URL + DB_FREE_GAME + gameName)
-            .then((value: AxiosResponse<IScenesDB>) => value.data)
+    private async getGame(gameName: string): Promise<IFreeGame> {
+        return Axios.get<IFreeGame>(SERVER_BASE_URL + DB_FREE_GAME + gameName)
+            .then((value: AxiosResponse<IFreeGame>) => value.data)
             // tslint:disable-next-line:no-any Since Axios defines reason as `any`
             .catch((reason: any) => {
                 throw new Error(reason.response.data.message);
