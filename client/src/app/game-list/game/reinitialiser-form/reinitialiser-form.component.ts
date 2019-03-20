48cff486f285fd4defe1c85c20e3fe5ae0db4a3e
Ophion <li.wenbo555@hotmail.com>
2019-03-20 10:52:15 -0400

    [dev] added reinitialiserForm popup

---
+++ client/src/app/game-list/game/reinitialiser-form/reinitialiser-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, Inject } from "@angular/core";
+import {MatDialogRef, MAT_DIALOG_DATA} from "@angular/material/dialog";
+import {Router} from "@angular/router";
+import {IDialogData} from "../dialog-data-interface/IDialogData";
+
+@Component({
+  selector: "app-reinitialiser-form",
+  templateUrl: "./reinitialiser-form.component.html",
+  styleUrls: ["./reinitialiser-form.component.css"],
+})
+export class ReinitialiserFormComponent {
+
+  public constructor (protected dialogRef: MatDialogRef<ReinitialiserFormComponent>,
+                      @Inject(MAT_DIALOG_DATA) public data: IDialogData,
+                      private router: Router) {/*vide*/}
+
+  public exit(message: Object = { status: "cancelled" }): void {
+    this.dialogRef.close(message);
+  }
+  public resetGame(): void {
+    /*call deleteGame from server*/
+    this.dialogRef.close();
+    this.router.navigate(["/admin/"]);
+  }
+}
