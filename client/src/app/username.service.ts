9de88866917527e3952a07fa27aa3790eee526d8
Philippe Ballandras <philippe.ballandras@hotmail.com>
2019-03-21 18:44:36 -0400

    [Dev] Removed http methods to register usernames un UNService

--- client/src/app/username.service.ts
+++ client/src/app/username.service.ts
@@ -1,10 +1,7 @@
-import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { of, Observable, Subscription } from "rxjs";
-import { catchError } from "rxjs/operators";
+import { Subscription } from "rxjs";
 import { WebsocketMessage } from "../../../common/communication/messages/message";
 import { UserValidationMessage } from "../../../common/communication/messages/user-validation-message";
-import { SERVER_BASE_URL, USERNAME_BASE } from "../../../common/communication/routes";
 import { SocketEvent } from "../../../common/communication/socket-events";
 import { SocketService } from "./socket.service";
 
@@ -12,7 +9,6 @@ import { SocketService } from "./socket.service";
 export class UNListService {
 
   public static username: string = "";
-  private static readonly BASE_URL: string = SERVER_BASE_URL + USERNAME_BASE;
   private readonly NON_ALPHANUMERIC_MESSAGE: string = "Tu dois utiliser seulement des caractères alphanumériques!";
   private readonly USERNAME_TOO_SHORT_MESSAGE: string = "Ton identifiant est trop court!";
   private readonly USERNAME_TAKEN_MESSAGE: string = "Cet identifiant est deja pris! Essaie un nouvel identifiant";
@@ -24,33 +20,12 @@ export class UNListService {
   public username: string;
   public response: UserValidationMessage;
 
-  public constructor(private http: HttpClient,
-                     private websocket: SocketService) {
-
+  public constructor(private websocket: SocketService) {
     this.minLength = this.USERNAME_MIN_LENGTH;
     this.message = "";
     this.username = "";
     this.response = {
       username: "", available: true,
-    };
-  }
-
-  public async sendReleaseRequest(): Promise<UserValidationMessage> {
-    return this.http.post<UserValidationMessage>(UNListService.BASE_URL + "/release", {
-      username: UNListService.username,
-      available: false,
-    }).toPromise<UserValidationMessage>();
-  }
-
-  public sendUserRequest(name: string): Observable<UserValidationMessage> {
-    return this.http.post<UserValidationMessage>(UNListService.BASE_URL + "/add", { username: name, available: false })
-      .pipe(catchError(this.handleError<UserValidationMessage>("sendUserRequest")));
-  }
-
-  private handleError<T>(request: string, result?: T): (error: Error) => Observable<T> {
-
-    return (error: Error): Observable<T> => {
-      return of(result as T);
     };
   }
 
@@ -75,28 +50,11 @@ export class UNListService {
     return false;
   }
 
-  public async validateName(name: string): Promise<boolean> {
-    if (this.isTooShort(name) || !this.isAlphanumeric(name)) {
-      return false;
-    }
-
-    return this.sendUserRequest(name).toPromise().then((response: UserValidationMessage) => {
-      this.response = response;
-      if (typeof this.response !== "undefined" && !this.response.available) {
-        this.message = this.USERNAME_TAKEN_MESSAGE;
-
-        return false;
-      }
-
-      return typeof this.response !== "undefined";
-    });
-  }
-
-  public checkAvailability(username: string, callback: (answer: boolean) => void): void {
+  public checkAvailability(username: string, callback: (answer: boolean) => void): boolean {
     if (this.isTooShort(username) || !this.isAlphanumeric(username)) {
       callback(false);
 
-      return;
+      return false;
     }
 
     const message: WebsocketMessage<string> = {
@@ -111,5 +69,7 @@ export class UNListService {
       callback(answer.body);
       sub.unsubscribe();
     });
+
+    return true;
   }
 }
