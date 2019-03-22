f75a3300548388d184ca0398ba29d68b7c222df6
Milen Bozhilov <milen_bozhilov@yahoo.ca>
2019-03-22 12:13:23 -0400

    [test] test of cheat mode

--- client/src/app/scene-creator/scene-renderer.service.spec.ts
+++ client/src/app/scene-creator/scene-renderer.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from "@angular/core/testing";
 import * as THREE from "three";
 import { ComponentNotLoadedError } from "../../../../common/errors/component.errors";
 import { SceneRendererService } from "./scene-renderer.service";
+import {IJson3DObject} from "../../../../common/free-game-json-interface/JSONInterface/IScenesJSON";
 
 describe("SceneRendererService", () => {
   beforeEach(() => {
@@ -34,6 +35,45 @@ describe("SceneRendererService", () => {
     service.loadScenes(original, modified);
     expect(service.scene).toBe(original);
     expect(service.modifiedScene).toBe(modified);
+  });
+
+  it("should make only diff objets blink when there is a blink event", async (done: DoneFn) => {
+    const service: SceneRendererService = TestBed.get(SceneRendererService);
+
+    const original: THREE.Scene = new THREE.Scene();
+    const modified: THREE.Scene = new THREE.Scene();
+
+    const diffObject = new THREE.Mesh();
+    const notDiffObject = new THREE.Mesh();
+    diffObject.translateX(12);
+    original.add(diffObject);
+    original.add(notDiffObject);
+
+    const oriCont: HTMLDivElement = (document.createElement("div")) as HTMLDivElement;
+    const modCont: HTMLDivElement = (document.createElement("div")) as HTMLDivElement;
+    service.init(oriCont, modCont);
+    service.loadScenes(original, modified);
+    service.modifyCheatState(() => {
+      return new Promise<IJson3DObject[]>((resolve) => {
+        resolve([<IJson3DObject>{position: [12, 0, 0]}]);
+      });
+    });
+    let updateThread: NodeJS.Timeout;
+    const failThread: NodeJS.Timeout = setTimeout(()=>{
+      clearInterval(updateThread);
+      done.fail();
+    }, 1000);
+    setInterval(()=>{
+      if (!original.children[0].visible && original.children[1].visible) {
+        clearTimeout(failThread);
+        clearInterval(updateThread);
+        done();
+      } else if (!original.children[1].visible) {
+        clearTimeout(failThread);
+        clearInterval(updateThread);
+        done.fail();
+      }
+    }, 20);
   });
 
   it("should reasign the new scenes at second call", () => {
