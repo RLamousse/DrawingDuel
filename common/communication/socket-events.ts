8bc3b750e46569c40ef8073762b53d3b25e30824
Ophion <li.wenbo555@hotmail.com>
2019-03-22 11:36:42 -0400

    [dev] added delete event to ws event

--- common/communication/socket-events.ts
+++ common/communication/socket-events.ts
@@ -2,6 +2,7 @@ export enum SocketEvent {
     CONNECTION = "connection",
     DISCONNECTION = "disconnection",
     WELCOME = "welcome",
+    DELETE = "delete",
     CHECK_DIFFERENCE = "check-diff",
     DUMMY = "dummy"
 }
