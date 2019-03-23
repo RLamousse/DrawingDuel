01e09e6e7073013d813c7de7b4e5ef0ac7c36be4
Philippe Ballandras <philippe.ballandras@hotmail.com>
2019-03-23 11:18:08 -0400

    [Test] Fixed ts lint problems in message-box tests

--- client/src/app/message-box/message-box.component.spec.ts
+++ client/src/app/message-box/message-box.component.spec.ts
@@ -1,9 +1,9 @@
 // tslint:disable: no-floating-promises
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { MatListModule } from "@angular/material";
+import {SocketEvent} from "../../../../common/communication/socket-events";
 import { SocketService } from "../socket.service";
 import { MessageBoxComponent } from "./message-box.component";
-import {SocketEvent} from "../../../../common/communication/socket-events";
 
 describe("MessageBoxComponent", () => {
   let component: MessageBoxComponent;
@@ -17,7 +17,7 @@ describe("MessageBoxComponent", () => {
       ],
       imports: [
         MatListModule,
-      ]
+      ],
     })
     .compileComponents();
   }));
@@ -32,12 +32,15 @@ describe("MessageBoxComponent", () => {
     expect(component).toBeTruthy();
   });
 
-  it("should receive the messages and add them", () => {
+  it("should receive the messages and add them", async () => {
+    // tslint:disable-next-line: no-any
     (component as any).handleChatEvent({
       title: SocketEvent.USER_CONNECTION,
       body: "BoiChadddd",
     });
 
+    // We are accessing private members
+    // tslint:disable-next-line: no-any
     return expect((component as any).messages.length).toBeGreaterThan(0);
   });
 });
