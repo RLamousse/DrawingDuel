cc9fb147a3a930483de4080ad983cbca30234f33
Anthony Potvin <DarkHoTs66@hotmail.com>
2019-03-23 17:22:12 -0400

    [D] error message appear and dissapear according to plan, position is hazardous

--- client/src/app/scene-creator/scene-creator.component.ts
+++ client/src/app/scene-creator/scene-creator.component.ts
@@ -1,6 +1,7 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from "@angular/core";
 import {ActivatedRoute} from "@angular/router";
 import {IFreeGame} from "../../../../common/model/game/free-game";
+import {IPoint} from "../../../../common/model/point";
 import {GameService} from "../game.service";
 import {IScene} from "../scene-interface";
 import {FreeGameCreatorService} from "./FreeGameCreator/free-game-creator.service";
@@ -11,9 +12,7 @@ export enum TextType {
   VICTORY,
 }
 
-export const IMAGE_DATA_PIXEL_LENGTH: number = 4;
-export const DEFAULT_CANVAS_HEIGHT: number = 480;
-export const TEXT_FONT: string = "30px Comic Sans MS";
+export const TEXT_FONT: string = "20px Comic Sans MS";
 export const ERROR_TEXT_COLOR: string = "#ff0000";
 export const VICTORY_TEXT_COLOR: string = "#008000";
 export const DEFAULT_TEXT_COLOR: string = "#000000";
@@ -90,23 +89,48 @@ export class SceneCreatorComponent implements AfterViewInit, OnInit {
         this.clickEnabled = true; })
         .catch(() => {
           this.cursorEnabled = false;
-          this.cursorStatusChange();
-          const canvasElm: HTMLElement| null = document.getElementById("originalCanvasMessage");
-          if (canvasElm === null) {
+          const canvasElm: HTMLElement | null = document.getElementById("originalCanvasMessage");
+          const canvasContext: CanvasRenderingContext2D | null = (canvasElm as HTMLCanvasElement).getContext("2d");
+          if (canvasContext === null) {
             return;
           }
-          const canvasContext: CanvasRenderingContext2D | null = (canvasElm as HTMLCanvasElement).getContext("2d");
-
+          canvasContext.font = TEXT_FONT;
+          canvasContext.textAlign = "center";
+          canvasContext.strokeStyle = "black";
+          const point: IPoint = {
+            x: $event.offsetX - canvasContext.canvas.getBoundingClientRect().left,
+            y: $event.offsetY - canvasContext.canvas.getBoundingClientRect().top,
+          };
+          console.log($event.clientY);
+          this.drawText(IDENTIFICATION_ERROR_TEXT, point, canvasContext, TextType.ERROR);
+          this.resetRoutine(canvasContext);
         });
     }
   }
 
-  private cursorStatusChange(): void {
+  private resetRoutine(ctx: CanvasRenderingContext2D): void {
     const TIMEOUT: number = 1000;
     setTimeout(() => {
       this.cursorEnabled = true;
       this.clickEnabled = true;
+      ctx.clearRect(0 , 0, ctx.canvas.width, ctx.canvas.height);
       },
                TIMEOUT);
+  }
+
+  public drawText(text: string, position: IPoint, ctx: CanvasRenderingContext2D, textType?: TextType): void {
+    switch (textType) {
+      case TextType.ERROR:
+        ctx.fillStyle = ERROR_TEXT_COLOR;
+        ctx.strokeText(text, position.x, position.y);
+        break;
+      case TextType.VICTORY:
+        ctx.fillStyle = VICTORY_TEXT_COLOR;
+        break;
+      default:
+        ctx.fillStyle = DEFAULT_TEXT_COLOR;
+        break;
+    }
+    ctx.fillText(text, position.x, position.y);
   }
 }
