383b6abfc8fefb1407a772164d5ad44aebe121d7
Philippe Ballandras <philippe.ballandras@hotmail.com>
2019-03-23 15:21:17 -0400

    [other] Removed comments in initial-view

--- client/src/app/initial-view/initial-view.component.ts
+++ client/src/app/initial-view/initial-view.component.ts
@@ -22,16 +22,6 @@ export class InitialViewComponent {
     this.handleUsernameAvailability = this.handleUsernameAvailability.bind(this);
   }
 
-  // public async updateUsername(): Promise<void> {
-  //   if (await this.userService.validateName(this.newUsername)) {
-  //     this.username = this.newUsername;
-  //     UNListService.username = this.username;
-  //     await this.router.navigate(["/game-list"]);
-  //   } else {
-  //     this.errorMessage = this.userService.message;
-  //   }
-  // }
-
   public updateUsername(): void {
     this.userService.checkAvailability(this.newUsername, this.handleUsernameAvailability);
   }
