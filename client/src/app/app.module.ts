f9c36bb3c4888e6d7a524d614821482747c5f069
Philippe Ballandras <philippe.ballandras@hotmail.com>
2019-03-23 11:36:42 -0400

    [other] Fixed lint error in app.module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -17,7 +17,7 @@ import {GameListComponent} from "./game-list/game-list.component";
 import {GameComponent} from "./game-list/game/game.component";
 import {GameService} from "./game.service";
 import {InitialViewComponent} from "./initial-view/initial-view.component";
-import { MessageBoxComponent } from './message-box/message-box.component';
+import { MessageBoxComponent } from "./message-box/message-box.component";
 import {PlayViewComponent} from "./play-view/play-view.component";
 import {Form3DService} from "./scene-creator/3DFormService/3-dform.service";
 import {FreeGameCreatorService} from "./scene-creator/FreeGameCreator/free-game-creator.service";
