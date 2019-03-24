11ceadd6548d35351cc982c16d56151e49b75eb5
Ophion <li.wenbo555@hotmail.com>
2019-03-23 22:40:47 -0400

    [test] added done in place of async

--- client/src/app/await-view/await-view.component.spec.ts
+++ client/src/app/await-view/await-view.component.spec.ts
@@ -7,15 +7,19 @@ describe('AwaitViewComponent', () => {
   let component: AwaitViewComponent;
   let fixture: ComponentFixture<AwaitViewComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AwaitViewComponent ]
+  beforeEach((done) => {
+    TestBed.configureTestingModule(
+      {
     })
     .compileComponents();
   }));
         providers: [
         { provide: Router, useClass: class { public navigate: jasmine.Spy = jasmine.createSpy("navigate"); }, },
       ],
+    });
+    done();
+
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AwaitViewComponent);
