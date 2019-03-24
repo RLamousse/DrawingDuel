614b4e10d01eb2c2d49d1cd07c53514780c80919
Ophion <li.wenbo555@hotmail.com>
2019-03-23 22:33:36 -0400

    [test] added providers to supprimer-form component spec

--- client/src/app/game-list/game/supprimer-form/supprimer-form.component.spec.ts
+++ client/src/app/game-list/game/supprimer-form/supprimer-form.component.spec.ts
@@ -1,14 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from "@angular/common/http";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA  } from "@angular/material/dialog";
+import { Router } from "@angular/router";
+import { GameService } from "../../../game.service";
+import { FreeGameCreatorService } from "../../../scene-creator/FreeGameCreator/free-game-creator.service";
+import { FreeGamePhotoService } from "../../../scene-creator/free-game-photo-service/free-game-photo.service";
+import { SocketService } from "../../../socket.service";
+import { SupprimerFormComponent } from "./supprimer-form.component";
 
-import { SupprimerFormComponent } from './supprimer-form.component';
-
-describe('SupprimerFormComponent', () => {
+describe("SupprimerFormComponent", () => {
   let component: SupprimerFormComponent;
   let fixture: ComponentFixture<SupprimerFormComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SupprimerFormComponent ]
+      declarations: [ SupprimerFormComponent ],
+      imports: [ MatDialogModule, HttpClientModule],
+      providers: [{ provide: Router, useClass: class { public navigate: jasmine.Spy = jasmine.createSpy("navigate"); } },
+                  {provide: MatDialogRef, useValue: {}},
+                  {provide: MAT_DIALOG_DATA, useValue: {}, },
+                  SocketService, GameService, FreeGamePhotoService, FreeGameCreatorService ],
     })
     .compileComponents();
   }));
@@ -19,7 +30,7 @@ describe('SupprimerFormComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
