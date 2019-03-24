798785b8a917e578dc79ac566a75455430a2ef55
Milen Bozhilov <milen_bozhilov@yahoo.ca>
2019-03-24 18:29:54 -0400

    [test] created the tests for the UpdateGameScoresWebsocketActionService

---
+++ server/app/services/websocket/update-game-scores-websocket-action.service.spec.ts
@@ -0,0 +1,91 @@
+/* tslint:disable:max-file-line-count */
+// tslint:disable:no-magic-numbers
+import Axios from "axios";
+import AxiosAdapter from "axios-mock-adapter";
+// tslint:disable-next-line:no-duplicate-imports Weird interaction between singletons and interface (olivier st-o approved)
+import MockAdapter from "axios-mock-adapter";
+import {expect} from "chai";
+import * as HttpStatus from "http-status-codes";
+import {Socket} from "socket.io";
+import {anything, instance, mock, when} from "ts-mockito";
+import {
+    ChatMessage,
+    ChatMessagePlayerCount, ChatMessagePosition,
+    ChatMessageType,
+    UpdateScoreMessage,
+    WebsocketMessage
+} from "../../../../common/communication/messages/message";
+import {MODIFY_SCORES, SERVER_BASE_URL} from "../../../../common/communication/routes";
+import {SocketEvent} from "../../../../common/communication/socket-events";
+import {ChatWebsocketActionService} from "./chat-websocket-action.service";
+import {UpdateGameScoresWebsocketActionService} from "./update-game-scores-websocket-action.service";
+import {IllegalArgumentError, ScoreNotGoodEnough} from "../../../../common/errors/services.errors";
+
+describe("Update Game Scores Websocket Action Service", () => {
+
+    let axiosMock: MockAdapter;
+    let mockedChatWebsocketActionService: ChatWebsocketActionService;
+
+    const getMockedService: () => UpdateGameScoresWebsocketActionService = () => {
+        return new UpdateGameScoresWebsocketActionService(
+            instance(mockedChatWebsocketActionService),
+        );
+    };
+
+    beforeEach(() => {
+        axiosMock = new AxiosAdapter(Axios);
+
+        mockedChatWebsocketActionService = mock(ChatWebsocketActionService);
+        when(mockedChatWebsocketActionService.execute(anything(), anything())).thenReturn();
+    });
+
+    it("should do nothing if the micro service throws a ScoreNotGoodEnough error", async () => {
+
+        axiosMock.onPut(SERVER_BASE_URL + MODIFY_SCORES)
+            .reply(HttpStatus.INTERNAL_SERVER_ERROR, new ScoreNotGoodEnough());
+        when(mockedChatWebsocketActionService.execute(anything(), anything())).thenThrow();
+        const message: WebsocketMessage<UpdateScoreMessage> = {title: SocketEvent.UPDATE_SCORE,
+                                                               body: {isSolo: true,
+                                                                      gameName: "someGame",
+                                                                      newTime: {name: "someGuy", time: 123}}};
+
+        return getMockedService().execute(message, {} as Socket).catch(() => {
+            expect.fail();
+        });
+    });
+
+    it("should throw if micro service throws any other error than ScoreNotGoodEnough error", async () => {
+
+        axiosMock.onPut(SERVER_BASE_URL + MODIFY_SCORES)
+            .reply(HttpStatus.INTERNAL_SERVER_ERROR, new IllegalArgumentError());
+        when(mockedChatWebsocketActionService.execute(anything(), anything())).thenReturn();
+        const message: WebsocketMessage<UpdateScoreMessage> = {title: SocketEvent.UPDATE_SCORE,
+                                                               body: {isSolo: true,
+                                                                      gameName: "someGame",
+                                                                      newTime: {name: "someGuy", time: 123}}};
+
+        return getMockedService().execute(message, {} as Socket).catch((error: Error) => {
+            expect(error.message).to.eql(IllegalArgumentError.ARGUMENT_ERROR_MESSAGE );
+        });
+    });
+
+    it("should send a message if everything is fine", async () => {
+
+        axiosMock.onPut(SERVER_BASE_URL + MODIFY_SCORES)
+            .reply(HttpStatus.OK, 1);
+        when(mockedChatWebsocketActionService.execute(anything(), anything())).thenCall(
+            (data: WebsocketMessage<ChatMessage>, socket: Socket) => {
+                expect(data.body).to.contain({type: ChatMessageType.BEST_TIME,
+                    gameName: "someGame",
+                    playerName: "someGuy",
+                    playerCount: ChatMessagePlayerCount.SOLO,
+                    position: ChatMessagePosition.FIRST,
+                });
+            });
+        const message: WebsocketMessage<UpdateScoreMessage> = {title: SocketEvent.UPDATE_SCORE,
+            body: {isSolo: true,
+                gameName: "someGame",
+                newTime: {name: "someGuy", time: 123}}};
+        await getMockedService().execute(message, {} as Socket);
+    });
+});
