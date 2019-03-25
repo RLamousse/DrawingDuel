d53456d506d4a21870b9b55e60156da9344fe2b8
Ophion <li.wenbo555@hotmail.com>
2019-03-25 17:50:41 -0400

    [dev] added call to reset game time

--- client/src/app/game-list/game/reset-game-form/reset-game-form.component.ts
+++ client/src/app/game-list/game/reset-game-form/reset-game-form.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from "@angular/core";
 import {MatDialogRef, MAT_DIALOG_DATA} from "@angular/material/dialog";
 import {Router} from "@angular/router";
 import {IDialogData} from "../../../../../../common/dialog-data-interface/IDialogData";
+import {GameService} from "../../../game.service";
 
 @Component({
   selector: "app-reset-game-form",
@@ -12,13 +13,15 @@ export class ResetGameFormComponent {
 
   public constructor (protected dialogRef: MatDialogRef<ResetGameFormComponent>,
                       @Inject(MAT_DIALOG_DATA) public data: IDialogData,
-                      private router: Router) {}
+                      private router: Router,
+                      private gameService: GameService) {}
 
   public exit(message: Object = { status: "cancelled" }): void {
     this.dialogRef.close(message);
+    window.location.reload();
   }
   public resetGame(): void {
-    /*call rsesetGame from server*/
+    this.gameService.resetGameTime(this.data.gameName);
     this.dialogRef.close();
     this.router.navigate(["/admin/"]) // tslint:disable-next-line:no-any Generic error response
     .catch((reason: any) => {
