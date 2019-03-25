10ac68476f4176e315e008a768bf0267a787079b
Ophion <li.wenbo555@hotmail.com>
2019-03-25 17:52:30 -0400

    [dev] reload after reset

--- client/src/app/game-list/game/game.component.ts
+++ client/src/app/game-list/game/game.component.ts
@@ -4,6 +4,7 @@ import {Router} from "@angular/router";
 import {IRecordTime} from "../../../../../common/model/game/record-time";
 import {DeleteGameFormComponent} from "./delete-game-form/delete-game-form.component";
 import {ResetGameFormComponent} from "./reset-game-form/reset-game-form.component";
+
 @Component({
   selector: "app-game",
   templateUrl: "./game.component.html",
@@ -12,7 +13,7 @@ import {ResetGameFormComponent} from "./reset-game-form/reset-game-form.componen
 
 export class GameComponent {
 
-  public constructor(private router: Router, private dialog: MatDialog) {}
+  private isDeleted: boolean;
 
   @Input() public gameName: string = "test";
   @Input() public bestSoloTimes: IRecordTime[];
@@ -37,18 +38,18 @@ export class GameComponent {
 
   protected rightButtonClick(): void {
     if (this.rightButton === "joindre") {
-      this.navigateAwait();
+      this.isDeleted ? this.openGameDeletedDialog() : this.navigateAwait();
     } else if (this.rightButton === "reinitialiser") {
       const dialogConfig: MatDialogConfig = new MatDialogConfig();
       dialogConfig.autoFocus = true;
       dialogConfig.data = {gameName: this.gameName, isSimpleGame: this.isSimpleGame};
-      this.dialog.open(ResetGameFormComponent, dialogConfig);
+      this.dialog.open(ResetGameFormComponent, dialogConfig).afterClosed().subscribe(() => window.location.reload());
     }
   }
 
   private navigatePlayView(): void {
    this.router.navigate(["/play-view/"], {queryParams: {
-      gameName: this.gameName, originalImage: this.originalImage, modifiedImage: this.modifiedImage },
+      gameName: this.gameName, originalImage: this.originalImage, modifiedImage: this.modifiedImage, isDeleted: this.isDeleted },
     })
       // tslint:disable-next-line:no-any Generic error response
      .catch((reason: any) => {
@@ -59,7 +60,7 @@ export class GameComponent {
   private navigateFreeView(): void {
     this.router.navigate(["/3d-view/"], {
       queryParams: {
-        gameName: this.gameName,
+        gameName: this.gameName, isDeleted: this.isDeleted,
       },
     })
       // tslint:disable-next-line:no-any Generic error response
