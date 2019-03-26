1dba80ac7e9ad1eafea295c0a582d21efdc4a2fc
Ophion <li.wenbo555@hotmail.com>
2019-03-26 14:32:29 -0400

    [reformat] reformated folder spaces

--- client/src/app/play-view/play-view.component.spec.ts
+++ client/src/app/play-view/play-view.component.spec.ts
@@ -28,17 +28,15 @@ describe("PlayViewComponent", () => {
           MatListModule,
         ],
         providers: [SocketService,
-                    { provide: Router,
-                      useClass: class { public navigate: jasmine.Spy = jasmine.createSpy("navigate"); },   },
-                    {
-          provide: ActivatedRoute,
-          useValue: {queryParams: {
-            subscribe: (fn: (queryParams: string ) => void) => fn(
-              // tslint:disable-next-line:max-line-length
-              "play-view?gameName=numbers&originalImage=https:%2F%2Fi.imgur.com%2Fvc0cKmB.png&modifiedImage=https:%2F%2Fi.imgur.com%2F5lei5Nb.png"
-              ,
-            ),
-        }, } , },
+                    { provide: Router, useClass: class { public navigate: jasmine.Spy = jasmine.createSpy("navigate"); },   },
+                    { provide: ActivatedRoute,
+                      useValue: {queryParams: {
+                          subscribe: (fn: (queryParams: string ) => void) => fn(
+                          // tslint:disable-next-line:max-line-length
+                          "play-view?gameName=numbers&originalImage=https:%2F%2Fi.imgur.com%2Fvc0cKmB.png&modifiedImage=https:%2F%2Fi.imgur.com%2F5lei5Nb.png", ),
+                        },
+                      },
+                    },
 
       ],
     });
