83570de6f26e8adf91153277ce364b2298f4ffe7
Philippe Rivest <technophil98@gmail.com>
2019-03-26 17:42:57 -0400

    [B] Updated tests for client

--- client/src/app/simple-game/simple-game.service.spec.ts
+++ client/src/app/simple-game/simple-game.service.spec.ts
@@ -4,8 +4,7 @@ import MockAdapter from "axios-mock-adapter";
 // tslint:disable-next-line:no-duplicate-imports Weird interaction between singletons and interface (olivier st-o approved)
 import AxiosAdapter from "axios-mock-adapter";
 import * as HttpStatus from "http-status-codes";
-import {IDiffValidatorControllerResponse} from "../../../../common/communication/responses/diff-validator-controller.response";
-import {DIFF_VALIDATOR_BASE, SERVER_BASE_URL} from "../../../../common/communication/routes";
+import {DB_SIMPLE_GAME, DIFF_VALIDATOR_BASE, SERVER_BASE_URL} from "../../../../common/communication/routes";
 import {AlreadyFoundDifferenceError, NoDifferenceAtPointError} from "../../../../common/errors/services.errors";
 import {DifferenceCluster, DIFFERENCE_CLUSTER_POINTS_INDEX} from "../../../../common/model/game/simple-game";
 import {ORIGIN} from "../../../../common/model/point";
@@ -14,13 +13,28 @@ import {SimpleGameService} from "./simple-game.service";
 describe("SimpleGameService", () => {
 
   let axiosMock: MockAdapter;
-  const CONTROLLER_BASE_URL: string = SERVER_BASE_URL + DIFF_VALIDATOR_BASE;
-  const ALL_GET_CALLS_REGEX: RegExp = new RegExp(`${CONTROLLER_BASE_URL}/*`);
+  let service: SimpleGameService;
+  const DIFF_VALIDATOR_URL: string = SERVER_BASE_URL + DIFF_VALIDATOR_BASE;
+  const DIFF_VALIDATOR_GET_CALLS_REGEX: RegExp = new RegExp(`${DIFF_VALIDATOR_URL}/*`);
+  const DB_URL: string = SERVER_BASE_URL + DB_SIMPLE_GAME;
+  const DB_GET_CALLS_REGEX: RegExp = new RegExp(`${DB_URL}/*`);
 
   beforeEach(() => {
     axiosMock = new AxiosAdapter(Axios);
 
+    axiosMock.onGet(DB_GET_CALLS_REGEX)
+      .reply(HttpStatus.OK, {diffData: [0, [ORIGIN]]});
+
     return TestBed.configureTestingModule({});
+  });
+
+  beforeEach(() => {
+    service = TestBed.get(SimpleGameService);
+    service.gameName = "SimpleGameService-Test";
+    // @ts-ignore Mock certain properties of object
+    service["_game"] = {
+      diffData: [[0, [ORIGIN]]],
+    };
   });
 
   it("should be created", () => {
@@ -29,10 +43,7 @@ describe("SimpleGameService", () => {
   });
 
   it("should throw when there's no difference at point", async () => {
-    const service: SimpleGameService = TestBed.get(SimpleGameService);
-    service.gameName = "SimpleGameService-Test";
-
-    axiosMock.onGet(ALL_GET_CALLS_REGEX)
+    axiosMock.onGet(DIFF_VALIDATOR_GET_CALLS_REGEX)
       .reply(HttpStatus.NOT_FOUND);
 
     return service.validateDifferenceAtPoint(ORIGIN)
@@ -42,10 +53,7 @@ describe("SimpleGameService", () => {
   });
 
   it("should throw on unexpected server response", async () => {
-    const service: SimpleGameService = TestBed.get(SimpleGameService);
-    service.gameName = "SimpleGameService-Test";
-
-    axiosMock.onGet(ALL_GET_CALLS_REGEX)
+    axiosMock.onGet(DIFF_VALIDATOR_GET_CALLS_REGEX)
       .reply(HttpStatus.INTERNAL_SERVER_ERROR);
 
     return service.validateDifferenceAtPoint(ORIGIN)
@@ -55,16 +63,8 @@ describe("SimpleGameService", () => {
   });
 
   it("should return a difference cluster with successful call to server", async () => {
-    const service: SimpleGameService = TestBed.get(SimpleGameService);
-    service.gameName = "SimpleGameService-Test";
-
-    axiosMock.onGet(ALL_GET_CALLS_REGEX)
-      .reply(HttpStatus.OK,
-             {
-               differenceClusterCoords: [ORIGIN],
-               differenceClusterId: 0,
-             } as IDiffValidatorControllerResponse,
-      );
+    axiosMock.onGet(DIFF_VALIDATOR_GET_CALLS_REGEX)
+      .reply(HttpStatus.OK);
 
     return service.validateDifferenceAtPoint(ORIGIN)
       .then((differenceCluster: DifferenceCluster) => {
@@ -73,16 +73,8 @@ describe("SimpleGameService", () => {
   });
 
   it("should throw if a difference cluster was already found", async () => {
-    const service: SimpleGameService = TestBed.get(SimpleGameService);
-    service.gameName = "SimpleGameService-Test";
-
-    axiosMock.onGet(ALL_GET_CALLS_REGEX)
-      .reply(HttpStatus.OK,
-             {
-               differenceClusterCoords: [ORIGIN],
-               differenceClusterId: 0,
-             } as IDiffValidatorControllerResponse,
-      );
+    axiosMock.onGet(DIFF_VALIDATOR_GET_CALLS_REGEX)
+      .reply(HttpStatus.OK);
 
     await service.validateDifferenceAtPoint(ORIGIN);
 
@@ -93,16 +85,8 @@ describe("SimpleGameService", () => {
   });
 
   it("should update the difference count", async (done) => {
-    const service: SimpleGameService = TestBed.get(SimpleGameService);
-    service.gameName = "SimpleGameService-Test";
-
-    axiosMock.onGet(ALL_GET_CALLS_REGEX)
-      .reply(HttpStatus.OK,
-             {
-               differenceClusterCoords: [ORIGIN],
-               differenceClusterId: 0,
-             } as IDiffValidatorControllerResponse,
-      );
+    axiosMock.onGet(DIFF_VALIDATOR_GET_CALLS_REGEX)
+      .reply(HttpStatus.OK);
 
     service.foundDifferencesCount.subscribe((diffCount: number) => {
       expect(diffCount).toEqual(1);
