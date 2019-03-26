87ccb3ea743b1e70b9bb956f150d1a16d849d9e5
Anthony Potvin <DarkHoTs66@hotmail.com>
2019-03-26 16:09:33 -0400

    [D]photoService changed, still not showing thematic scene

--- client/src/app/scene-creator/free-game-photo-service/free-game-photo.service.ts
+++ client/src/app/scene-creator/free-game-photo-service/free-game-photo.service.ts
@@ -9,9 +9,13 @@ export class FreeGamePhotoService {
   private readonly backGroundColor: number = 0x0B7B90;
   private readonly renderSize: number = 400;
   private readonly cameraZ: number = 200;
+  private divElemt: HTMLDivElement;
+
+  public constructor() {
+    this.divElemt = (document.createElement("div")) as HTMLDivElement;
+  }
 
   public takePhoto(scene: THREE.Scene): string {
-    const divElem: HTMLDivElement = (document.createElement("div")) as HTMLDivElement;
     const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(
       this.fieldOfView,
       1,
@@ -24,10 +28,10 @@ export class FreeGamePhotoService {
     renderer.setClearColor(this.backGroundColor);
     renderer.setPixelRatio(devicePixelRatio);
     renderer.setSize(this.renderSize, this.renderSize);
-    divElem.appendChild(renderer.domElement);
+    this.divElemt.appendChild(renderer.domElement);
 
     renderer.render(scene, camera);
 
-    return (divElem.children[0] as HTMLCanvasElement).toDataURL();
+    return (this.divElemt.children[0] as HTMLCanvasElement).toDataURL();
   }
 }
