0ba03baf15fbaacb6b15e924d4fcab09488b508f
Anthony Potvin <anthony.potvin@polymtl.ca>
2019-03-29 10:55:23 -0400

    [D]added function compute direction

--- client/src/app/scene-creator/objectCollisionService/object-collision.service.ts
+++ client/src/app/scene-creator/objectCollisionService/object-collision.service.ts
@@ -22,7 +22,30 @@ export class ObjectCollisionService {
     this.modifiedObjects = modified;
   }
 
-  public computeCollision(): boolean {
+  public computeCollision(velocity: THREE.Vector3): boolean {
     return !!(this.camera && this.originalObjects && this.modifiedObjects);
+    const direction = this.computeDirection(velocity);
+
+  }
+
+  private computeDirection(velocity: THREE.Vector3): THREE.Vector3 {
+    const direction: THREE.Vector3 = new THREE.Vector3(0 , 0, 0);
+    if (velocity.x < 0) {
+      direction.x = -1;
+    } else if (velocity.x > 0) {
+      direction.x = 1;
+    }
+    if (velocity.y < 0) {
+      direction.y = -1;
+    } else if (velocity.y > 0) {
+      direction.y = 1;
+    }
+    if (velocity.z < 0) {
+      direction.z = -1;
+    } else if (velocity.z > 0) {
+      direction.z = 1;
+    }
+
+    return direction;
   }
 }
