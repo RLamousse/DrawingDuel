e3327b3e92e0a50d07d3465f97eb3fd10c4e552d
Anthony Potvin <anthony.potvin@polymtl.ca>
2019-03-29 10:12:43 -0400

    [D]added constructor implementation + setParameters function

--- client/src/app/scene-creator/objectCollisionService/object-collision.service.ts
+++ client/src/app/scene-creator/objectCollisionService/object-collision.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import * as THREE from "three";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ObjectCollisionService {
 
-  constructor() { }
+  private camera: THREE.Camera;
+  private originalObjects: THREE.Object3D[];
+  private modifiedObjects: THREE.Object3D[];
+
+  public constructor() {
+    this.camera = new THREE.Camera();
+    this.originalObjects = [];
+    this.modifiedObjects = [];
+  }
+
+  public setCollision(camera: THREE.Camera, original: THREE.Object3D[], modified: THREE.Object3D[]): void {
+    this.camera = camera;
+    this.originalObjects = original;
+    this.modifiedObjects = modified;
+  }
 }
