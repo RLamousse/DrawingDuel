321035b4a076102c14804c3a68dabaceb1af02fe
Milen Bozhilov <milen_bozhilov@yahoo.ca>
2019-04-01 20:50:52 -0400

    [dev] completed game manager controller (based on data-base controller)

--- server/app/controllers/game-manager.controller.ts
+++ server/app/controllers/game-manager.controller.ts
@@ -1,7 +1,11 @@
-import {Router} from "express";
+import {NextFunction, Request, Response, Router} from "express";
+import * as Httpstatus from "http-status-codes";
 import {inject, injectable} from "inversify";
+import {NonExistentGameError} from "../../../common/errors/database.errors";
+import {ISimpleGame} from "../../../common/model/game/simple-game";
 import {DataBaseService} from "../services/data-base.service";
 import Types from "../types";
+import {executePromiseSafely} from "./controller-utils";
 
 @injectable()
 export class GameManagerController {
@@ -11,6 +15,72 @@ export class GameManagerController {
 
     public get router(): Router {
         const router: Router = Router();
+
+        // ┌──┬────────┬──┐
+        // │  │ SIMPLE │  │
+        // └──┴────────┴──┘
+
+        router.delete("/simple/delete/:id", async (req: Request, res: Response, next: NextFunction) => {
+            executePromiseSafely(res, next, async () => {
+                res.json(await this.dataBaseService.simpleGames.delete(req.params["id"]));
+            });
+        });
+
+        router.get("/simple/get-all", async (req: Request, res: Response, next: NextFunction) => {
+            executePromiseSafely(res, next, async () => {
+                res.json(await this.dataBaseService.simpleGames.getAll());
+            });
+        });
+
+        router.get("/simple/get-one/:id", async (req: Request, res: Response, next: NextFunction) => {
+            executePromiseSafely(res, next, async () => {
+                this.dataBaseService.simpleGames
+                    .getFromId(req.params["id"])
+                    .then((value: ISimpleGame) => {
+                        res.json(value);
+                    }).catch((reason: Error) => {
+                    if (reason.message === NonExistentGameError.NON_EXISTENT_GAME_ERROR_MESSAGE) {
+                        res.status(Httpstatus.NOT_FOUND);
+                    } else {
+                        res.status(Httpstatus.INTERNAL_SERVER_ERROR);
+                    }
+                    res.json(reason);
+                });
+            });
+        });
+
+        // ┌──┬──────┬──┐
+        // │  │ FREE │  │
+        // └──┴──────┴──┘
+        router.delete("/simple/delete/:id", async (req: Request, res: Response, next: NextFunction) => {
+            executePromiseSafely(res, next, async () => {
+                res.json(await this.dataBaseService.simpleGames.delete(req.params["id"]));
+            });
+        });
+
+        router.get("/simple/get-all", async (req: Request, res: Response, next: NextFunction) => {
+            executePromiseSafely(res, next, async () => {
+                res.json(await this.dataBaseService.simpleGames.getAll());
+            });
+        });
+
+        router.get("/simple/get-one/:id", async (req: Request, res: Response, next: NextFunction) => {
+            executePromiseSafely(res, next, async () => {
+                this.dataBaseService.simpleGames
+                    .getFromId(req.params["id"])
+                    .then((value: ISimpleGame) => {
+                        res.json(value);
+                    }).catch((reason: Error) => {
+                    if (reason.message === NonExistentGameError.NON_EXISTENT_GAME_ERROR_MESSAGE) {
+                        res.status(Httpstatus.NOT_FOUND);
+                    } else {
+                        res.status(Httpstatus.INTERNAL_SERVER_ERROR);
+                    }
+                    res.json(reason);
+                });
+            });
+        });
+
         return router;
     }
 }
