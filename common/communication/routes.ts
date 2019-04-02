5a12d9489e6f3993e175433def16594ede8b8d8a
Milen Bozhilov <milen_bozhilov@yahoo.ca>
2019-04-02 13:28:57 -0400

    [ref] removed db controller routes

--- common/communication/routes.ts
+++ common/communication/routes.ts
@@ -4,7 +4,6 @@ export const USERNAME_BASE: string = "/api/usernames/";
 export const DIFF_VALIDATOR_BASE: string = "/api/diff-validator/";
 export const DIFF_CREATOR_BASE: string = "/api/image-diff/";
 export const GAME_CREATOR_BASE: string = "/api/game-creator/";
-export const DB_BASE: string = "/api/data-base/";
 export const GAME_MANAGER_BASE: string = "/api/game-manager/";
 export const SCORE_TABLE_UPDATE: string = "/api/score-table/";
 
@@ -13,9 +12,6 @@ export const RESET_SCORES: string = SCORE_TABLE_UPDATE + "reset-scores/";
 
 export const SIMPLE_GAME_CREATION_ROUTE: string = GAME_CREATOR_BASE + "create-simple-game/";
 export const FREE_GAME_CREATION_ROUTE: string = GAME_CREATOR_BASE + "create-free-game/";
-
-export const DB_SIMPLE_GAME: string = DB_BASE + "games/simple/";
-export const DB_FREE_GAME: string = DB_BASE + "games/free/";
 
 export const GAME_MANAGER_SIMPLE: string = GAME_MANAGER_BASE + "simple/";
 export const GAME_MANAGER_FREE: string = GAME_MANAGER_BASE + "free/";
