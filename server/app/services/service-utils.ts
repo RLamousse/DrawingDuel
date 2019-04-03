caa3bd1cb2bd63b35436330b1e8bc2c5eb723926
Robin Canton Lamousse <robin.canton-lamousse@polymtl.ca>
2019-04-03 12:08:12 -0400

    [UI] username max lenght change and default username change to fit

--- server/app/services/service-utils.ts
+++ server/app/services/service-utils.ts
@@ -3,16 +3,12 @@ import {IRecordTime} from "../../../common/model/game/record-time";
 
 const MIN_GENERATED_SCORE: number = 120;
 const MAX_GENERATED_SCORE: number = 360;
-const GENERATED_NAMES: string[] = ["normie",
-                                   "hardTryer4269",
-                                   "xXx_D4B0W5_xXx",
-                                   "spongebob",
-                                   "pikatchu",
-                                   "sanic",
-                                   "Donald J. Trump",
-                                   "some aliens",
-                                   "ur mom",
-                                   "Som Ting Wong"];
+const GENERATED_NAMES: string[] = ["BotAnthony",
+                                   "BotMilen",
+                                   "BotPhilB",
+                                   "BotWenBo",
+                                   "BotPhilR",
+                                   "BotRobin"];
 
 const generateRandomNames: () => string[] = (): string[]  =>  {
     const randomNamesIndex: number[] = [];
