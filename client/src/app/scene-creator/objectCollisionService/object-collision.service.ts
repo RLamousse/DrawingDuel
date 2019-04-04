6fae72a79cd2d1292899ef1702dcd5c37242c1e5
Anthony Potvin <anthony.potvin@polymtl.ca>
2019-04-04 17:01:19 -0400

    [D] collision detection now on point

--- client/src/app/scene-creator/objectCollisionService/object-collision.service.ts
+++ client/src/app/scene-creator/objectCollisionService/object-collision.service.ts
@@ -10,26 +10,24 @@ export class ObjectCollisionService {
     new THREE.Vector3(0, 0, -1),
     new THREE.Vector3(1, 0, 0),
     new THREE.Vector3(-1, 0, 0),
+    new THREE.Vector3(0, 1, 0),
+    new THREE.Vector3(0, -1, 0),
     new THREE.Vector3(1, 0, 1),
-    new THREE.Vector3(-1, 0, 1),
-    new THREE.Vector3(1, 0, -1),
     new THREE.Vector3(-1, 0, -1),
+    new THREE.Vector3(1, 0, -1),
+    new THREE.Vector3(-1, 0, 1),
     ];
 
-  private readonly collisionDist: number = 30;
-
-  public constructor() {
-  }
+  private readonly collisionDist: number = 25;
 
   public raycastCollision(camera: THREE.Camera, oriObjs: THREE.Object3D[], modObjs: THREE.Object3D[], vel: THREE.Vector3): void {
     for (const vec of this.directionVecs) {
-      const ray: THREE.Raycaster = new THREE.Raycaster(camera.position.clone(), vec.normalize().clone());
+      const vecClone: THREE.Vector3 = vec.clone().applyQuaternion(camera.quaternion);
+      const ray: THREE.Raycaster = new THREE.Raycaster(camera.position.clone(), vecClone.normalize());
       const interOri: THREE.Intersection[] = ray.intersectObjects(oriObjs, true);
-      const interMod: THREE.Intersection[] = ray.intersectObjects(oriObjs, true);
-      if (this.isCollision(interOri)) {
-        this.cancelVelocity(vel, vec);
-      } else if (this.isCollision(interMod)) {
-        this.cancelVelocity(vel, vec);
+      const interMod: THREE.Intersection[] = ray.intersectObjects(modObjs, true);
+      if (this.isCollision(interOri) || this.isCollision(interMod)) {
+        this.cancelVelocity(vel, vecClone, vec);
       }
     }
   }
@@ -37,15 +35,22 @@ export class ObjectCollisionService {
     return (inter.length > 0 && inter[0].distance < this.collisionDist);
   }
 
-  private cancelVelocity(vel: THREE.Vector3, dir: THREE.Vector3): void {
-    if (vel.x < 0 && dir.x < 0) {
+  private cancelVelocity(vel: THREE.Vector3, dirColl: THREE.Vector3, oriDir: THREE.Vector3): void {
+    const comparisonVec: THREE.Vector3 = dirColl.clone();
+    if (oriDir.x - dirColl.x < -1 || oriDir.x - dirColl.x > 1) {
+      comparisonVec.x = -comparisonVec.x;
+    }
+    if (oriDir.z - dirColl.z < -1 || oriDir.z - dirColl.z > 1) {
+      comparisonVec.z = -comparisonVec.z;
+    }
+    if (vel.x < 0 && comparisonVec.x < 0) {
       vel.x = 0;
-    } else if (vel.x > 0 && dir.x > 0) {
+    } else if (vel.x > 0 && comparisonVec.x > 0) {
       vel.x = 0;
     }
-    if (vel.z < 0 && dir.z < 0) {
+    if (vel.z < 0 && comparisonVec.z < 0) {
       vel.z = 0;
-    } else if (vel.z > 0 && dir.z > 0) {
+    } else if (vel.z > 0 && comparisonVec.z > 0) {
       vel.z = 0;
     }
   }
