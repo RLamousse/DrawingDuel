c8b77a1b3285150729d692dc071797ed6d57a646
Anthony Potvin <anthony.potvin@polymtl.ca>
2019-04-04 20:16:37 -0400

    [T]added test to check if cancelVel is not called

--- client/src/app/scene-creator/objectCollisionService/object-collision.service.spec.ts
+++ client/src/app/scene-creator/objectCollisionService/object-collision.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from "@angular/core/testing";
+import * as THREE from "three";
 // tslint:disable:no-magic-numbers
 import { ObjectCollisionService } from "./object-collision.service";
 
@@ -8,5 +9,19 @@ describe("ObjectCollisionService", () => {
   it("should be created", () => {
     const service: ObjectCollisionService = TestBed.get(ObjectCollisionService);
     expect(service).toBeTruthy();
+  });
+
+  // Test rayCastCollision
+  //  public raycastCollision(camera: THREE.Camera, oriObjs: THREE.Object3D[], modObjs: THREE.Object3D[], vel: THREE.Vector3): void {
+  it ("should not call cancelVelocity if noObject in collision", () => {
+    const service: ObjectCollisionService = TestBed.get(ObjectCollisionService);
+    spyOn(service as any, "isCollision").and.returnValue(false);
+    spyOn(service as any, "cancelVelocity").and.callThrough();
+    const ori: THREE.Object3D[] = [];
+    const mod: THREE.Object3D[] = [];
+    const cam: THREE.Camera = new THREE.Camera();
+    const vel: THREE.Vector3 = new THREE.Vector3();
+    service.raycastCollision(cam, ori, mod, vel);
+    expect(service["cancelVelocity"]).not.toHaveBeenCalled();
   });
 });
