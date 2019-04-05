020298fa9bd9309a3ea26ff368d6f31230ee05ab
Anthony Potvin <anthony.potvin@polymtl.ca>
2019-04-04 20:51:32 -0400

    [T]added test for cancelVelocity, change the x or z sign value

--- client/src/app/scene-creator/objectCollisionService/object-collision.service.spec.ts
+++ client/src/app/scene-creator/objectCollisionService/object-collision.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from "@angular/core/testing";
 import * as THREE from "three";
 // tslint:disable:no-magic-numbers
+// tslint:disable:no-any
 import { ObjectCollisionService } from "./object-collision.service";
 
 describe("ObjectCollisionService", () => {
@@ -12,7 +13,6 @@ describe("ObjectCollisionService", () => {
   });
 
   // Test rayCastCollision
-  //  public raycastCollision(camera: THREE.Camera, oriObjs: THREE.Object3D[], modObjs: THREE.Object3D[], vel: THREE.Vector3): void {
   it ("should not call cancelVelocity if noObject in collision", () => {
     const service: ObjectCollisionService = TestBed.get(ObjectCollisionService);
     spyOn(service as any, "isCollision").and.returnValue(false);
@@ -23,5 +23,56 @@ describe("ObjectCollisionService", () => {
     const vel: THREE.Vector3 = new THREE.Vector3();
     service.raycastCollision(cam, ori, mod, vel);
     expect(service["cancelVelocity"]).not.toHaveBeenCalled();
+  });
+  it("should call cancelVelocity when there is a collision", () => {
+    const service: ObjectCollisionService = TestBed.get(ObjectCollisionService);
+    spyOn(service as any, "isCollision").and.returnValue(true);
+    spyOn(service as any, "cancelVelocity").and.callThrough();
+    const ori: THREE.Object3D[] = [];
+    const mod: THREE.Object3D[] = [];
+    const cam: THREE.Camera = new THREE.Camera();
+    const vel: THREE.Vector3 = new THREE.Vector3();
+    service.raycastCollision(cam, ori, mod, vel);
+    expect(service["cancelVelocity"]).toHaveBeenCalled();
+  });
+  it("should switch the sign of comparisonVec.x when ori.x - dir.x < -1", () => {
+    const service: ObjectCollisionService = TestBed.get(ObjectCollisionService);
+    const vel: THREE.Vector3 = new THREE.Vector3(0, 0, 0);
+    const oriDir: THREE.Vector3 = new THREE.Vector3(-1, 0, 0);
+    const dirColl: THREE.Vector3 = new THREE.Vector3(0.8, 0, 0);
+    service["cancelVelocity"](vel, dirColl, oriDir);
+    expect(service["comparisonVec"].x).toEqual(-dirColl.x);
+  });
+  it("should switch the sign of comparisonVec.x when ori.x - dir.x > 1", () => {
+    const service: ObjectCollisionService = TestBed.get(ObjectCollisionService);
+    const vel: THREE.Vector3 = new THREE.Vector3(0, 0, 0);
+    const oriDir: THREE.Vector3 = new THREE.Vector3(1, 0, 0);
+    const dirColl: THREE.Vector3 = new THREE.Vector3(-0.8, 0, 0);
+    service["cancelVelocity"](vel, dirColl, oriDir);
+    expect(service["comparisonVec"].x).toEqual(-dirColl.x);
+  });
+  it("should switch the sign of comparisonVec.z when ori.z - dir.z < -1", () => {
+    const service: ObjectCollisionService = TestBed.get(ObjectCollisionService);
+    const vel: THREE.Vector3 = new THREE.Vector3(0, 0, 0);
+    const oriDir: THREE.Vector3 = new THREE.Vector3(1, 0, -1);
+    const dirColl: THREE.Vector3 = new THREE.Vector3(-0.8, 0, 0.7);
+    service["cancelVelocity"](vel, dirColl, oriDir);
+    expect(service["comparisonVec"].z).toEqual(-dirColl.z);
+  });
+  it("should switch the sign of comparisonVec.z when ori.z - dir.z > 1", () => {
+    const service: ObjectCollisionService = TestBed.get(ObjectCollisionService);
+    const vel: THREE.Vector3 = new THREE.Vector3(0, 0, 0);
+    const oriDir: THREE.Vector3 = new THREE.Vector3(1, 0, 1);
+    const dirColl: THREE.Vector3 = new THREE.Vector3(-0.8, 0, -0.7);
+    service["cancelVelocity"](vel, dirColl, oriDir);
+    expect(service["comparisonVec"].z).toEqual(-dirColl.z);
+  });
+  it("should put to 0 vel.x when vel.x < 0 && comparisonVec.x < 0", () => {
+    const service: ObjectCollisionService = TestBed.get(ObjectCollisionService);
+    const vel: THREE.Vector3 = new THREE.Vector3(-1, 0, 0);
+    const oriDir: THREE.Vector3 = new THREE.Vector3(0, 0, 0);
+    const dirColl: THREE.Vector3 = new THREE.Vector3(-0.8, 0, 0);
+    service["cancelVelocity"](vel, dirColl, oriDir);
+    expect(vel.x).toEqual(0);
   });
 });
--- client/src/app/scene-creator/objectCollisionService/object-collision.service.ts
+++ client/src/app/scene-creator/objectCollisionService/object-collision.service.ts
@@ -17,9 +17,13 @@ export class ObjectCollisionService {
     new THREE.Vector3(1, 0, -1),
     new THREE.Vector3(-1, 0, 1),
     ];
-
   private readonly collisionDist: number = 25;
 
+  private comparisonVec: THREE.Vector3;
+
+  public constructor() {
+    this.comparisonVec = new THREE.Vector3();
+  }
   public raycastCollision(camera: THREE.Camera, oriObjs: THREE.Object3D[], modObjs: THREE.Object3D[], vel: THREE.Vector3): void {
     for (const vec of this.directionVecs) {
       const vecClone: THREE.Vector3 = vec.clone().applyQuaternion(camera.quaternion);
@@ -36,21 +40,21 @@ export class ObjectCollisionService {
   }
 
   private cancelVelocity(vel: THREE.Vector3, dirColl: THREE.Vector3, oriDir: THREE.Vector3): void {
-    const comparisonVec: THREE.Vector3 = dirColl.clone();
+    this.comparisonVec = dirColl.clone();
     if (oriDir.x - dirColl.x < -1 || oriDir.x - dirColl.x > 1) {
-      comparisonVec.x = -comparisonVec.x;
+      this.comparisonVec.x = -this.comparisonVec.x;
     }
     if (oriDir.z - dirColl.z < -1 || oriDir.z - dirColl.z > 1) {
-      comparisonVec.z = -comparisonVec.z;
+      this.comparisonVec.z = -this.comparisonVec.z;
     }
-    if (vel.x < 0 && comparisonVec.x < 0) {
+    if (vel.x < 0 && this.comparisonVec.x < 0) {
       vel.x = 0;
-    } else if (vel.x > 0 && comparisonVec.x > 0) {
+    } else if (vel.x > 0 && this.comparisonVec.x > 0) {
       vel.x = 0;
     }
-    if (vel.z < 0 && comparisonVec.z < 0) {
+    if (vel.z < 0 && this.comparisonVec.z < 0) {
       vel.z = 0;
-    } else if (vel.z > 0 && comparisonVec.z > 0) {
+    } else if (vel.z > 0 && this.comparisonVec.z > 0) {
       vel.z = 0;
     }
   }
