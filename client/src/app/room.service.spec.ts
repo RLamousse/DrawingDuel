bd05a4090b1025f2517cdea4c0417fd49766b672
Philippe Ballandras <philippe.ballandras@hotmail.com>
2019-04-07 13:20:31 -0400

    [other] Fixed import path after move (refactor)

--- client/src/app/room.service.spec.ts
+++ client/src/app/room.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from "@angular/core/testing";
 
-import {SocketService} from "../../socket.service";
+import {SocketService} from "./socket.service";
 import { RoomService } from "./room.service";
 
 describe("RoomService", () => {
