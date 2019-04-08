d737468c97f57498a351ea515961d1016e64fa7d
Milen Bozhilov <milen_bozhilov@yahoo.ca>
2019-04-08 13:24:55 -0400

    [dev] Deleting games with toBeDeleted flag on server start

--- server/app/services/data-base.service.ts
+++ server/app/services/data-base.service.ts
@@ -1,7 +1,8 @@
 import * as config from "config";
 import { injectable } from "inversify";
-import {Db, MongoClient} from "mongodb";
+import {Db, FilterQuery, MongoClient} from "mongodb";
 import "reflect-metadata";
+import {IGame} from "../../../common/model/game/game";
 import { FreeGamesCollectionService } from "./db/free-games.collection.service";
 import { SimpleGamesCollectionService } from "./db/simple-games.collection.service";
 
@@ -16,6 +17,7 @@ export class DataBaseService {
     private readonly DB_PASSWORD: string = config.get("mongo.password");
     private readonly DB_DB: string = config.get("mongo.database");
     private readonly DB_URL: string = `mongodb+srv://${this.DB_USER}:${this.DB_PASSWORD}@cluster0-ijbac.mongodb.net/test?retryWrites=true`;
+    private readonly  TO_BE_DELETED_FILTER_QUERY: FilterQuery<IGame> = {["toBeDeleted"]: {$eq: true}};
 
     private _dataBase: Db;
     private _simpleGames: SimpleGamesCollectionService;
@@ -23,10 +25,11 @@ export class DataBaseService {
 
     public constructor() {
         MongoClient.connect(this.DB_URL, {useNewUrlParser: true})
-            .then((client: MongoClient) => {
+            .then(async (client: MongoClient) => {
                 this._dataBase = client.db(this.DB_DB);
                 this._simpleGames = new SimpleGamesCollectionService(this._dataBase.collection(SIMPLE_GAMES_COLLECTION));
                 this._freeGames = new FreeGamesCollectionService(this._dataBase.collection(FREE_GAMES_COLLECTION));
+                await this.cleanGamesToBeDeleted();
             })
             .catch(() => {
                 console.error("Unable to connect to the database!");
@@ -43,5 +46,10 @@ export class DataBaseService {
 
     public get freeGames(): FreeGamesCollectionService {
         return this._freeGames;
+    }
+
+    private async cleanGamesToBeDeleted(): Promise<void> {
+        await this.simpleGames.deleteSelected(this.TO_BE_DELETED_FILTER_QUERY);
+        await this.freeGames.deleteSelected(this.TO_BE_DELETED_FILTER_QUERY);
     }
 }
