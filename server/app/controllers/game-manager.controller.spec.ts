c849c39321d931271380eb7d91d8b287a4bc96c7
Milen Bozhilov <milen_bozhilov@yahoo.ca>
2019-04-08 16:49:35 -0400

    [test] added tests for the new routes in game manager

--- server/app/controllers/game-manager.controller.spec.ts
+++ server/app/controllers/game-manager.controller.spec.ts
@@ -6,7 +6,7 @@ import * as request from "supertest";
 import {anything, instance, mock, when} from "ts-mockito";
 import {Message} from "../../../common/communication/messages/message";
 import {
-    GAME_MANAGER_FREE, GAME_MANAGER_GET_ALL_REQUEST, GAME_MANAGER_GET_REQUEST,
+    GAME_MANAGER_FREE, GAME_MANAGER_GET_ALL_REQUEST, GAME_MANAGER_GET_NOT_DELETED_REQUEST, GAME_MANAGER_GET_REQUEST,
     GAME_MANAGER_SIMPLE,
 } from "../../../common/communication/routes";
 import {DatabaseError, NonExistentGameError} from "../../../common/errors/database.errors";
@@ -36,10 +36,12 @@ describe("Data-base controller", () => {
 
         when(mockSimpleGames.delete(anything())).thenResolve(SUCCESS_MESSAGE);
         when(mockSimpleGames.getAll()).thenResolve([]);
+        when(mockSimpleGames.getAllWithQuery(anything())).thenResolve([]);
         when(mockSimpleGames.getFromId(anything())).thenResolve();
 
         when(mockFreeGames.delete(anything())).thenResolve(SUCCESS_MESSAGE);
         when(mockFreeGames.getAll()).thenResolve([]);
+        when(mockFreeGames.getAllWithQuery(anything())).thenResolve([]);
         when(mockFreeGames.getFromId(anything())).thenResolve();
 
         container.rebind(types.DataBaseService).toConstantValue(instance(mockDataBaseService));
@@ -47,9 +49,17 @@ describe("Data-base controller", () => {
     });
 
     describe("Simple Games", () => {
-        it("should send a success message on get all", async () => {
+        it("should send empty array get all", async () => {
             return request(app)
                 .get(GAME_MANAGER_SIMPLE + GAME_MANAGER_GET_ALL_REQUEST)
+                .expect(HttpStatus.OK)
+                .then((response) => {
+                    expect(response.body).to.eql([]);
+                });
+        });
+        it("should send a empty array on get not deleted", async () => {
+            return request(app)
+                .get(GAME_MANAGER_SIMPLE + GAME_MANAGER_GET_NOT_DELETED_REQUEST)
                 .expect(HttpStatus.OK)
                 .then((response) => {
                     expect(response.body).to.eql([]);
@@ -77,9 +87,17 @@ describe("Data-base controller", () => {
     });
 
     describe("Free Games", () => {
-        it("should send a success message on get all", async () => {
+        it("should send a empty array on get all", async () => {
             return request(app)
                 .get(GAME_MANAGER_FREE + GAME_MANAGER_GET_ALL_REQUEST)
+                .expect(HttpStatus.OK)
+                .then((response) => {
+                    expect(response.body).to.eql([]);
+                });
+        });
+        it("should send a empty array on get all", async () => {
+            return request(app)
+                .get(GAME_MANAGER_FREE + GAME_MANAGER_GET_NOT_DELETED_REQUEST)
                 .expect(HttpStatus.OK)
                 .then((response) => {
                     expect(response.body).to.eql([]);
