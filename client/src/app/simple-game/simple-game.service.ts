a123dfdd4ee300e2fdd86918b40dc3b8d1432709
Philippe Ballandras <philippe.ballandras@hotmail.com>
2019-04-11 16:30:08 -0400

    [Dev]Cleaned up simple-game.service after callback commit

--- client/src/app/simple-game/simple-game.service.ts
+++ client/src/app/simple-game/simple-game.service.ts
@@ -1,16 +1,7 @@
 import {Injectable} from "@angular/core";
-import Axios, {AxiosResponse} from "axios";
 import {Observable, Subject, Subscription} from "rxjs";
 import {createWebsocketMessage, WebsocketMessage} from "../../../../common/communication/messages/message";
-import {GAME_MANAGER_SIMPLE, SERVER_BASE_URL} from "../../../../common/communication/routes";
 import {SocketEvent} from "../../../../common/communication/socket-events";
-import {NonExistentGameError} from "../../../../common/errors/database.errors";
-// import {AlreadyFoundDifferenceError, NoDifferenceAtPointError} from "../../../../common/errors/services.errors";
-// import {ISimpleGameState} from "../../../../common/model/game/game-state";
-import {
-  // DIFFERENCE_CLUSTER_POINTS_INDEX,
-  ISimpleGame
-} from "../../../../common/model/game/simple-game";
 import {IPoint} from "../../../../common/model/point";
 import {ISimpleGameInteractionData, ISimpleGameInteractionResponse} from "../../../../common/model/rooms/interaction";
 import {SocketService} from "../socket.service";
@@ -20,32 +11,9 @@ import {SocketService} from "../socket.service";
             })
 export class SimpleGameService {
 
-  // private _game: ISimpleGame;
-  // private _gameState: ISimpleGameState;
-  private _gameName: string;
   private _differenceCountSubject: Subject<number> = new Subject();
 
-  public constructor(private socket: SocketService) {
-    // this._gameState = {
-    //   foundDifferenceClusters: [],
-    // };
-  }
-
-  public set gameName(value: string) {
-    this._gameName = value;
-
-    this.getGame()
-      .then((game: ISimpleGame) => {
-        // this._game = game;
-      })
-      .catch((error: Error) => {
-        throw error;
-      });
-
-    // this._gameState = {
-    //   foundDifferenceClusters: [],
-    // };
-  }
+  public constructor(private socket: SocketService) {}
 
   public get foundDifferencesCount(): Observable<number> {
     return this._differenceCountSubject;
@@ -68,69 +36,7 @@ export class SimpleGameService {
     }));
   }
 
-  // public async validateDifferenceAtPoint(point: IPoint): Promise<DifferenceCluster> {
-  //   this.assertAlreadyFoundDifference(point);
-  //
-  //   return Axios.get(
-  //     SERVER_BASE_URL + DIFF_VALIDATOR_BASE,
-  //     {
-  //       params: {
-  //         coordX: point.x,
-  //         coordY: point.y,
-  //         gameName: this._gameName,
-  //       } as IDiffValidatorControllerRequest,
-  //     })
-  //     .then(() => {
-  //       playRandomSound(FOUND_DIFFERENCE_SOUNDS);
-  //
-  //       return this.updateGameState(point);
-  //     })
-  //     // tslint:disable-next-line:no-any Generic error response
-  //     .catch((reason: any) => {
-  //       if (reason.response && reason.response.status === Httpstatus.NOT_FOUND) {
-  //         throw new NoDifferenceAtPointError();
-  //       }
-  //
-  //       throw new AbstractServiceError(reason.message);
-  //     });
-  // }
-
-  // private assertAlreadyFoundDifference(point: IPoint): void {
-  //   if (this.wasDifferenceFound(point)) {
-  //     throw new AlreadyFoundDifferenceError();
-  //   }
-  // }
-
-  // private wasDifferenceFound(point: IPoint): boolean {
-  //   return this._gameState.foundDifferenceClusters
-  //     .some((cluster: DifferenceCluster) =>
-  //             cluster[DIFFERENCE_CLUSTER_POINTS_INDEX]
-  //               .some((p: IPoint) => p.x === point.x && p.y === point.y));
-  // }
-
-  private async getGame(): Promise<ISimpleGame> {
-    return Axios.get<ISimpleGame>(SERVER_BASE_URL + GAME_MANAGER_SIMPLE + this._gameName)
-      .then((value: AxiosResponse<ISimpleGame>) => value.data)
-      // tslint:disable-next-line:no-any Since Axios defines reason as `any`
-      .catch(() => {
-        throw new NonExistentGameError();
-      });
+  public updateCounter(): void {
+    this._differenceCountSubject.next();
   }
-
-  // private updateGameState(clickedPoint: IPoint): DifferenceCluster {
-  //   const differenceCluster: DifferenceCluster | undefined = this._game.diffData
-  //     .find((cluster: DifferenceCluster) =>
-  //             cluster[DIFFERENCE_CLUSTER_POINTS_INDEX]
-  //               .some((point: IPoint) => point.x === clickedPoint.x && point.y === clickedPoint.y));
-  //
-  //   if (differenceCluster === undefined) {
-  //     throw new NoDifferenceAtPointError();
-  //   }
-  //
-  //   this._gameState.foundDifferenceClusters.push(differenceCluster);
-  //   this._differenceCountSubject.next(this._gameState.foundDifferenceClusters.length);
-  //
-  //   return differenceCluster;
-  // }
-
 }
