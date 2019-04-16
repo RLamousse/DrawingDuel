f6fcd22d2a904898e62af097af1bf50387f1abd2
Philippe Rivest <technophil98@gmail.com>
2019-04-15 23:37:41 -0400

    [D] Added toIVector3

--- client/src/app/util/client-utils.ts
+++ client/src/app/util/client-utils.ts
@@ -2,8 +2,14 @@ import {Vector3} from "three";
 import {IVector3} from "../../../../common/model/point";
 import {deepCompare} from "../../../../common/util/util";
 
+export const toIVector3:
+  (vector: Vector3) => IVector3 =
+  (vector: Vector3) => {
+    return {x: vector.x, y: vector.y, z: vector.z};
+  };
+
 export const compareToThreeVector3:
   (x: IVector3, y: Vector3) => boolean =
   (x: IVector3, y: Vector3) => {
-    return deepCompare(x, {x: y.x, y: y.y, z: y.z} as IVector3);
+    return deepCompare(x, toIVector3(y));
   };
