65ee58117ad6f4e29de7dc0a4506356824e80644
Philippe Ballandras <philippe.ballandras@hotmail.com>
2019-04-18 11:38:21 -0400

    [Test] Changed tests to match new ChatAction implementation

--- server/app/services/websocket/chat-websocket-action.service.spec.ts
+++ server/app/services/websocket/chat-websocket-action.service.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import {Server} from "socket.io";
 import {
-    createWebsocketMessage, ChatMessage,
+    ChatMessage,
     ChatMessagePosition, ChatMessageType,
     WebsocketMessage
 } from "../../../../common/communication/messages/message";
@@ -9,19 +9,7 @@ import { SocketEvent } from "../../../../common/communication/socket-events";
 import {OnlineType} from "../../../../common/model/game/game";
 import { ChatWebsocketActionService } from "./chat-websocket-action.service";
 import {RadioTowerService} from "./radio-tower.service";
-
-class Socket {
-    public constructor () {
-        this.emitValue = "";
-        this.eventValue = "";
-    }
-    public eventValue: string;
-    public emitValue: string;
-    public emit(event: string, message: WebsocketMessage<string>): void {
-        this.eventValue = event;
-        this.emitValue = message.body;
-    }
-}
+import {RoomNotDefinedError} from "../../../../common/errors/services.errors";
 
 class FakeServer {
 
@@ -70,7 +58,6 @@ class FakeServer {
 describe("ChatWebsocketActionService", () => {
 
     let service: ChatWebsocketActionService;
-    let socket: Socket;
     let radioTower: RadioTowerService;
     let server: FakeServer;
     beforeEach(() => {
@@ -78,7 +65,6 @@ describe("ChatWebsocketActionService", () => {
         radioTower = new RadioTowerService();
         radioTower.server = server as unknown as Server;
         service = new ChatWebsocketActionService(radioTower);
-        socket = new Socket();
 
         // Private member access
         // tslint:disable-next-line: no-any
@@ -88,103 +74,34 @@ describe("ChatWebsocketActionService", () => {
     });
 
     it("should emit an appropriate connection message on socket connection", () => {
-        const message: WebsocketMessage<ChatMessage> = createWebsocketMessage(
-            {
-                gameName: "",
-                playerCount: OnlineType.SOLO,
-                playerName: "Maxime",
-                position: ChatMessagePosition.NA,
-                timestamp: new Date("Sat Mar 23 2019 13:51:46 GMT-0400 (Eastern Daylight Time)"),
-                type: ChatMessageType.CONNECTION,
-            });
-        service.execute(message, socket as unknown as SocketIO.Socket);
+        const message: ChatMessage = {
+            gameName: "",
+            playerCount: OnlineType.SOLO,
+            playerName: "Maxime",
+            position: ChatMessagePosition.NA,
+            timestamp: new Date("Sat Mar 23 2019 13:51:46 GMT-0400 (Eastern Daylight Time)"),
+            type: ChatMessageType.CONNECTION,
+        };
+        service.sendChat(message);
         expect(server.eventValue).to.equal(SocketEvent.CHAT);
         expect(server.emitValue).to.equal("12:51:46 – Maxime vient de se connecter.");
     });
 
     it("should emit an appropriate disconnection message on socket disconnection", () => {
-        const message: WebsocketMessage<ChatMessage> = createWebsocketMessage(
-            {
-                gameName: "",
-                playerCount: OnlineType.SOLO,
-                playerName: "Maxime",
-                position: ChatMessagePosition.NA,
-                timestamp: new Date("Sat Mar 23 2019 13:51:46 GMT-0400 (Eastern Daylight Time)"),
-                type: ChatMessageType.DISCONNECTION,
-            });
-        service.execute(message, socket as unknown as SocketIO.Socket);
+        const message: ChatMessage = {
+            gameName: "",
+            playerCount: OnlineType.SOLO,
+            playerName: "Maxime",
+            position: ChatMessagePosition.NA,
+            timestamp: new Date("Sat Mar 23 2019 13:51:46 GMT-0400 (Eastern Daylight Time)"),
+            type: ChatMessageType.DISCONNECTION,
+        };
+        service.sendChat(message);
         expect(server.eventValue).to.equal(SocketEvent.CHAT);
         expect(server.emitValue).to.equal("12:51:46 – Maxime vient de se déconnecter.");
     });
 
     it("should emit an appropriate difference found message", () => {
-        const message: WebsocketMessage<ChatMessage> = createWebsocketMessage(
-            {
-                gameName: "",
-                playerCount: OnlineType.SOLO,
-                playerName: "Maxime",
-                position: ChatMessagePosition.NA,
-                timestamp: new Date("Sat Mar 23 2019 13:51:46 GMT-0400 (Eastern Daylight Time)"),
-                type: ChatMessageType.DIFF_FOUND,
-            });
-        service.execute(message, socket as unknown as SocketIO.Socket);
-        expect(server.eventValue).to.equal(SocketEvent.CHAT);
-        expect(server.emitValue).to.equal("12:51:46 – Différence trouvée.");
-        message.body.playerCount = OnlineType.MULTI;
-        service.execute(message, socket as unknown as SocketIO.Socket);
-        expect(server.eventValue).to.equal(SocketEvent.CHAT);
-        expect(server.emitValue).to.equal("12:51:46 – Différence trouvée par Maxime.");
-    });
-
-    it("should emit an appropriate difference error message", () => {
-        const message: WebsocketMessage<ChatMessage> = createWebsocketMessage({
-                gameName: "",
-                playerCount: OnlineType.SOLO,
-                playerName: "Maxime",
-                position: ChatMessagePosition.NA,
-                timestamp: new Date("Sat Mar 23 2019 13:51:46 GMT-0400 (Eastern Daylight Time)"),
-                type: ChatMessageType.DIFF_ERROR,
-                                                                              });
-        service.execute(message, socket as unknown as SocketIO.Socket);
-        expect(server.eventValue).to.equal(SocketEvent.CHAT);
-        expect(server.emitValue).to.equal("12:51:46 – Erreur.");
-        message.body.playerCount = OnlineType.MULTI;
-        service.execute(message, socket as unknown as SocketIO.Socket);
-        expect(server.eventValue).to.equal(SocketEvent.CHAT);
-        expect(server.emitValue).to.equal("12:51:46 – Erreur par Maxime.");
-    });
-
-    it("should emit an appropriate new time record message", () => {
-        const message: WebsocketMessage<ChatMessage> = createWebsocketMessage({
-                gameName: "MicheDePain",
-                playerCount: OnlineType.SOLO,
-                playerName: "Maxime",
-                position: ChatMessagePosition.FIRST,
-                timestamp: new Date("Sat Mar 23 2019 13:51:46 GMT-0400 (Eastern Daylight Time)"),
-                type: ChatMessageType.BEST_TIME,
-                                                                              });
-        service.execute(message, socket as unknown as SocketIO.Socket);
-        expect(server.eventValue).to.equal(SocketEvent.CHAT);
-        expect(server.emitValue).to.equal("12:51:46 – Maxime obtient la première place dans"
-                                        + " les meilleurs temps du jeu MicheDePain en solo.");
-    });
-
-    it("should emit a broken message when input was broken", () => {
-        // To test an impossible case
-        // tslint:disable-next-line: no-any
-        const message: WebsocketMessage<any> = createWebsocketMessage({
-                gameName: "MicheDePain",
-                playerCount: OnlineType.SOLO,
-                playerName: "Maxime",
-                position: ChatMessagePosition.FIRST,
-                timestamp: new Date("Sat Mar 23 2019 13:51:46 GMT-0400 (Eastern Daylight Time)"),
-                                                                      });
-        service.execute(message, socket as unknown as SocketIO.Socket);
-        expect(server.eventValue).to.equal(SocketEvent.CHAT);
-        expect(server.emitValue).to.equal("12:51:46 – Voici pourquoi les default existent dans les switchs.");
-    });
-
-    it("should emit an appropriate difference found message (using sendChat)", () => {
         const message: ChatMessage = {
             gameName: "",
             playerCount: OnlineType.SOLO,
@@ -193,16 +110,16 @@ describe("ChatWebsocketActionService", () => {
             timestamp: new Date("Sat Mar 23 2019 13:51:46 GMT-0400 (Eastern Daylight Time)"),
             type: ChatMessageType.DIFF_FOUND,
         };
-        service.sendChat(message, "");
+        service.sendChat(message, "id");
         expect(server.eventValue).to.equal(SocketEvent.CHAT);
         expect(server.emitValue).to.equal("12:51:46 – Différence trouvée.");
         message.playerCount = OnlineType.MULTI;
-        service.sendChat(message, "");
+        service.sendChat(message, "id");
         expect(server.eventValue).to.equal(SocketEvent.CHAT);
         expect(server.emitValue).to.equal("12:51:46 – Différence trouvée par Maxime.");
     });
 
-    it("should emit an appropriate difference error message (using sendChat)", () => {
+    it("should emit an appropriate difference error message", () => {
         const message: ChatMessage = {
             gameName: "",
             playerCount: OnlineType.SOLO,
@@ -211,13 +128,59 @@ describe("ChatWebsocketActionService", () => {
             timestamp: new Date("Sat Mar 23 2019 13:51:46 GMT-0400 (Eastern Daylight Time)"),
             type: ChatMessageType.DIFF_ERROR,
         };
-        service.sendChat(message, "");
+        service.sendChat(message, "id");
         expect(server.eventValue).to.equal(SocketEvent.CHAT);
         expect(server.emitValue).to.equal("12:51:46 – Erreur.");
         message.playerCount = OnlineType.MULTI;
-        service.sendChat(message, "");
+        service.sendChat(message, "id");
         expect(server.eventValue).to.equal(SocketEvent.CHAT);
         expect(server.emitValue).to.equal("12:51:46 – Erreur par Maxime.");
+    });
+
+    it("should emit an appropriate new time record message", () => {
+        const message: ChatMessage = {
+            gameName: "MicheDePain",
+            playerCount: OnlineType.SOLO,
+            playerName: "Maxime",
+            position: ChatMessagePosition.FIRST,
+            timestamp: new Date("Sat Mar 23 2019 13:51:46 GMT-0400 (Eastern Daylight Time)"),
+            type: ChatMessageType.BEST_TIME,
+        };
+        service.sendChat(message);
+        expect(server.eventValue).to.equal(SocketEvent.CHAT);
+        expect(server.emitValue).to.equal("12:51:46 – Maxime obtient la première place dans"
+                                        + " les meilleurs temps du jeu MicheDePain en solo.");
+    });
+
+    it("should emit a broken message when input was broken", () => {
+        // To test an impossible case
+        // tslint:disable-next-line: no-any
+        const message: any = {
+            gameName: "MicheDePain",
+            playerCount: OnlineType.SOLO,
+            playerName: "Maxime",
+            position: ChatMessagePosition.FIRST,
+            timestamp: new Date("Sat Mar 23 2019 13:51:46 GMT-0400 (Eastern Daylight Time)"),
+        };
+        service.sendChat(message);
+        expect(server.eventValue).to.equal(SocketEvent.CHAT);
+        expect(server.emitValue).to.equal("12:51:46 – Voici pourquoi les default existent dans les switchs.");
+    });
+
+    it("should throw if no room id specified and sending to a room", () => {
+        const message: ChatMessage = {
+            gameName: "",
+            playerCount: OnlineType.SOLO,
+            playerName: "Maxime",
+            position: ChatMessagePosition.NA,
+            timestamp: new Date("Sat Mar 23 2019 13:51:46 GMT-0400 (Eastern Daylight Time)"),
+            type: ChatMessageType.DIFF_FOUND,
+        };
+        try {
+            service.sendChat(message, "");
+        } catch (e) {
+            expect(e.message).to.equal(RoomNotDefinedError.ROOM_NOT_DEFINED_ERROR_MESSAGE);
+        }
     });
 
     it("should format time nicely", () => {
