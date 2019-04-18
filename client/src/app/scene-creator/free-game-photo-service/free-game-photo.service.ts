01b22b19e31fc764f126e2d9f8586dbba509e9d2
Anthony Potvin <anthony.potvin@polymtl.ca>
2019-04-17 20:15:26 -0400

    [D]made renderer and divElement to private, only one when creating games

--- client/src/app/scene-creator/free-game-photo-service/free-game-photo.service.ts
+++ client/src/app/scene-creator/free-game-photo-service/free-game-photo.service.ts
@@ -7,26 +7,36 @@ import {sleep} from "../../../../../common/util/util";
 import {IScene} from "../../scene-interface";
 import {FreeGameCreatorService} from "../FreeGameCreator/free-game-creator.service";
 
-@Injectable({
-  providedIn: "root",
-})
+@Injectable({providedIn: "root"})
 export class FreeGamePhotoService {
+
+  private divElem: HTMLDivElement;
+  private renderer: WebGLRenderer;
+  private readonly camera: PerspectiveCamera;
 
   public constructor(private creatorService: FreeGameCreatorService) {
     this.takePhoto = this.takePhoto.bind(this);
     FreeGamePhotoService.putThumbnail = FreeGamePhotoService.putThumbnail.bind(this);
+    this.divElem = (document.createElement("div")) as HTMLDivElement;
+    this.renderer = new WebGLRenderer({preserveDrawingBuffer: true});
+    this.camera = new PerspectiveCamera(
+      this.fieldOfView,
+      1,
+      this.nearClippingPane,
+      this.farClippingPane,
+    );
+    this.divElem.appendChild(this.renderer.domElement);
   }
   private readonly fieldOfView: number = 90;
   private readonly nearClippingPane: number = 1;
   private readonly farClippingPane: number = 1000;
   private readonly backGroundColor: number = 0x001A33;
-  private readonly renderSize: number = 100;
+  private readonly renderSize: number = 1000;
   private readonly cameraZ: number = 200;
   private readonly WAIT_TIME: number = 1000;
-  private readonly THUMBNAIL_QUALITY: number = 0.1;
+  private readonly THUMBNAIL_QUALITY: number = 0.5;
 
   private static putThumbnail(data: string, gameName: string): void {
-    console.log(data);
     Axios.put<IFreeGame>(SERVER_BASE_URL + GAME_MANAGER_FREE + encodeURIComponent(gameName), {thumbnail: data})
       .catch((err: Error) => {
         throw err;
@@ -34,26 +44,17 @@ export class FreeGamePhotoService {
   }
 
   public async takePhoto(gameName: string): Promise<void> {
-    const divElem: HTMLDivElement = (document.createElement("div")) as HTMLDivElement;
-    const camera: PerspectiveCamera = new PerspectiveCamera(
-      this.fieldOfView,
-      1,
-      this.nearClippingPane,
-      this.farClippingPane,
-    );
-    camera.position.set(0, 0, this.cameraZ);
-    const renderer: WebGLRenderer = new WebGLRenderer({preserveDrawingBuffer: true});
-    renderer.setClearColor(this.backGroundColor);
-    renderer.setPixelRatio(devicePixelRatio);
-    renderer.setSize(this.renderSize, this.renderSize);
-    divElem.appendChild(renderer.domElement);
+    this.camera.position.set(0, 0, this.cameraZ);
+    this.renderer.setClearColor(this.backGroundColor);
+    this.renderer.setPixelRatio(devicePixelRatio);
+    this.renderer.setSize(this.renderSize, this.renderSize);
 
     const scene: Scene = await this.getFreeGameScene(gameName);
-    renderer.render(scene, camera);
+    this.renderer.render(scene, this.camera);
     await sleep(this.WAIT_TIME);
-    renderer.render(scene, camera);
+    this.renderer.render(scene, this.camera);
 
-    const thumbnailData: string = (divElem.children[0] as HTMLCanvasElement).toDataURL("image/jpeg", this.THUMBNAIL_QUALITY);
+    const thumbnailData: string = (this.divElem.children[0] as HTMLCanvasElement).toDataURL("image/jpeg", this.THUMBNAIL_QUALITY);
     FreeGamePhotoService.putThumbnail(thumbnailData, gameName);
   }
 
