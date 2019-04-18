414d6466cf6fb5784d3294017aa39e33580bd220
Philippe Ballandras <philippe.ballandras@hotmail.com>
2019-04-18 11:37:05 -0400

    [Dev] Removed deprecated function+refactor

--- server/app/services/websocket/chat-websocket-action.service.ts
+++ server/app/services/websocket/chat-websocket-action.service.ts
@@ -1,6 +1,5 @@
 import {format} from "date-and-time";
 import {inject, injectable} from "inversify";
-import {Socket} from "socket.io";
 import {
     createWebsocketMessage,
     ChatMessage,
@@ -8,29 +7,41 @@ import {
     WebsocketMessage
 } from "../../../../common/communication/messages/message";
 import {SocketEvent} from "../../../../common/communication/socket-events";
+import {RoomNotDefinedError} from "../../../../common/errors/services.errors";
 import {OnlineType} from "../../../../common/model/game/game";
 import types from "../../types";
 import {RadioTowerService} from "./radio-tower.service";
-import {WebsocketActionService} from "./websocket-action.service";
 
 @injectable()
-export class ChatWebsocketActionService extends WebsocketActionService {
+export class ChatWebsocketActionService {
+
+    public constructor(@inject(types.RadioTowerService) private radioTower: RadioTowerService) {
+    }
 
     private readonly _EVENT_TYPE: SocketEvent = SocketEvent.CHAT;
     private readonly _DIFF_FOUND_BASE_MESSAGE: string = " – Différence trouvée";
     private readonly _DIFF_ERROR_BASE_MESSAGE: string = " – Erreur";
     private readonly _WRONG_INPUT_FORMAT: string = " – Voici pourquoi les default existent dans les switchs.";
 
-    public constructor(@inject(types.RadioTowerService) private radioTower: RadioTowerService) {
-        super();
+    private static formatTime(timestamp: Date): string {
+        const date: Date = new Date(timestamp);
+
+        return format(date, "HH:mm:ss");
     }
 
-    /**
-     * @deprecated
-     */
-    public execute(data: WebsocketMessage<ChatMessage>, socket: Socket, chatRoom?: string): void {
-        const message: WebsocketMessage<string> = this.generateMessage(data.body);
-        this.handleRightEmitScope(data.body, message, chatRoom);
+    public static getDisconnectionMessage(user: string): string {
+        return ` – ${user} vient de se déconnecter.`;
+    }
+
+    public static getConnectionMessage(user: string): string {
+        return ` – ${user} vient de se connecter.`;
+    }
+
+    private static getBestTimeMessage(data: ChatMessage): string {
+        return (
+            ` – ${data.playerName} obtient la ${data.position}`
+            + ` place dans les meilleurs temps du jeu ${data.gameName} en ${data.playerCount}.`
+        );
     }
 
     public sendChat(chatMessage: ChatMessage, chatRoom?: string): void {
@@ -54,7 +65,7 @@ export class ChatWebsocketActionService extends WebsocketActionService {
     }
 
     private generateMessage(data: ChatMessage): WebsocketMessage<string> {
-        let message: string = this.formatTime(data.timestamp);
+        let message: string = ChatWebsocketActionService.formatTime(data.timestamp);
         switch (data.type) {
             case ChatMessageType.DIFF_FOUND:
                 message += (this.getDiffFoundMessage(data));
@@ -63,13 +74,13 @@ export class ChatWebsocketActionService extends WebsocketActionService {
                 message += (this.getDiffErrorMessage(data));
                 break;
             case ChatMessageType.BEST_TIME:
-                message += (this.getBestTimeMessage(data));
+                message += (ChatWebsocketActionService.getBestTimeMessage(data));
                 break;
             case ChatMessageType.CONNECTION:
-                message += (this.getConnectionMessage(data.playerName));
+                message += (ChatWebsocketActionService.getConnectionMessage(data.playerName));
                 break;
             case ChatMessageType.DISCONNECTION:
-                message += (this.getDisconnectionMessage(data.playerName));
+                message += (ChatWebsocketActionService.getDisconnectionMessage(data.playerName));
                 break;
             default:
                 message += this._WRONG_INPUT_FORMAT;
@@ -77,21 +88,6 @@ export class ChatWebsocketActionService extends WebsocketActionService {
         }
 
         return createWebsocketMessage(message);
-    }
-
-    public getDisconnectionMessage(user: string): string {
-        return ` – ${user} vient de se déconnecter.`;
-    }
-
-    public getConnectionMessage(user: string): string {
-        return ` – ${user} vient de se connecter.`;
-    }
-
-    private getBestTimeMessage(data: ChatMessage): string {
-        return (
-            ` – ${data.playerName} obtient la ${data.position}`
-            + ` place dans les meilleurs temps du jeu ${data.gameName} en ${data.playerCount}.`
-        );
     }
 
     private getDiffErrorMessage(data: ChatMessage): string {
@@ -110,11 +106,5 @@ export class ChatWebsocketActionService extends WebsocketActionService {
         }
 
         return message += (".");
-    }
-
-    private formatTime(timestamp: Date): string {
-        const date: Date = new Date(timestamp);
-
-        return format(date, "HH:mm:ss");
     }
 }
