ebfd9ff1b2eaca0134a318ceda541e72ab894159
Milen Bozhilov <milen_bozhilov@yahoo.ca>
2019-04-18 23:06:01 -0400

    [D] created interface for params of get all

--- server/app/services/data-base.service.ts
+++ server/app/services/data-base.service.ts
@@ -11,6 +11,10 @@ export const FREE_GAMES_COLLECTION: string = "freeGames";
 export const NOT_TO_BE_DELETED_FILTER_QUERY: FilterQuery<IGame> = {["toBeDeleted"]: {$eq: false}};
 export const REMOVE_ID_PROJECTION_QUERY: Object = {_id: 0};
 export const REMOVE_DIFF_DATA_PROJECTION_QUERY: Object = {diffData: 0};
+export interface FindQuery {
+    filterQuery?: FilterQuery<IGame>;
+    projectQuery: Object;
+}
 
 @injectable()
 export class DataBaseService {
